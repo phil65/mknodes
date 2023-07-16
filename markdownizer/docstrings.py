b26362d11869ec5033850619cbe9c6a97bc8171e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-16 04:18:04 +0200

    chore: extend DocStrings

--- markdownizer/docstrings.py
+++ markdownizer/docstrings.py
@@ -5,6 +5,8 @@ import logging
 import os
 import types
 
+from typing import Any, Literal
+
 from markdownizer import classhelpers, markdownnode, utils
 
 
@@ -12,128 +14,100 @@ logger = logging.getLogger(__name__)
 
 
 class DocStrings(markdownnode.Text):
+    OPTIONS_DEFAULT = {}
+
     def __init__(
         self,
         obj: types.ModuleType | str | os.PathLike | type,
-        header: str = "",
         for_topmost: bool = False,
-        **kwargs,
+        allow_inspection: bool | None = None,
+        show_bases: bool | None = None,
+        show_source: bool | None = None,
+        preload_modules: list[str] | None = None,
+        heading_level: int | None = None,
+        show_root_heading: bool | None = None,
+        show_root_toc_entry: bool | None = None,
+        show_root_full_path: bool | None = None,
+        show_root_members_full_path: bool | None = None,
+        show_object_full_path: bool | None = None,
+        show_category_heading: bool | None = None,
+        show_symbol_type_heading: bool | None = None,
+        show_symbol_type_toc: bool | None = None,
+        members: list[str] | None = None,
+        members_order: Literal["alphabetical", "source"] | None = None,
+        filters: list[str] | None = None,
+        group_by_category: bool | None = None,
+        show_submodules: bool | None = None,
+        docstring_section_style: Literal["table", "list", "spacy"] | None = None,
+        merge_init_into_class: bool | None = None,
+        show_if_no_docstring: bool | None = None,
+        annotations_path: Literal["brief", "source"] | None = None,
+        line_length: int | None = None,
+        show_signature: bool | None = None,
+        show_signature_annotations: bool | None = None,
+        signature_crossrefs: bool | None = None,
+        separate_signature: bool | None = None,
+        **kwargs: Any,
     ):
         """Docstring section.
 
-        Possible keyword arguments:
+        Global options for DocStrings can be overridden by setting the keyword arguments
+        to not-None.
 
-        allow_inspection (bool): Whether to allow inspecting modules when visiting
-                                  them is not possible. Default: True.
-        show_bases (bool): Show the base classes of a class. Default: True.
-        show_source (bool): Show the source code of this object. Default: True.
-        preload_modules (list[str] | None): Pre-load modules
-
-        The modules must be listed as an array of strings. Default: None.
-
-        Headings options:
-
-        heading_level (int): The initial heading level to use. Default: 2.
-        show_root_heading (bool): Show the heading of the object at the root of the
-                                  documentation tree (i.e. the object referenced by
-                                  the identifier after :::). Default: False.
-        show_root_toc_entry (bool): If the root heading is not shown, at least
-                                    add a ToC entry for it. Default: True.
-        show_root_full_path (bool): Show the full Python path for the root
-                                    object heading. Default: True.
-        show_root_members_full_path (bool): Show the full Python path of the
-                                            root members. Default: False.
-        show_object_full_path (bool): Show the full Python path of every object.
-                                      Default: False.
-        show_category_heading (bool): When grouped by categories, show a heading
-                                      for each category. Default: False.
-        show_symbol_type_heading (bool): Show the symbol type in headings (e.g. mod,
-                                         class, func and attr). Default: False.
-        show_symbol_type_toc (bool): Show the symbol type in the Table of
-                                     Contents (e.g. mod, class, func and attr).
-                                     Default: False.
-        Members options:
-
-        members (list[str] | False | None): An explicit list of members to render.
-                                            Default: None.
-        members_order (str): The members ordering to use.
-                             Options: alphabetical - order by the members names,
-                             source - order members as they appear in the
-                             source file. Default: "alphabetical".
-        filters (list[str] | None): A list of filters applied to filter objects
-                                    based on their name. A filter starting with !
-                                    will exclude matching objects instead of
-                                    including them. The members option takes
-                                    precedence over filters (filters will still be
-                                    applied recursively to lower members in the
-                                    hierarchy). Default: ["!^_[^_]"].
-        group_by_category (bool): Group the object's children by categories:
-                                  attributes, classes, functions, and modules.
-                                  Default: True.
-        show_submodules (bool): When rendering a module, show its submodules
-                                recursively. Default: False.
-        DocStrings options:
-
-        docstring_style (str): The docstring style to use: google, numpy, sphinx,
-                               or None. Default: "google".
-        docstring_options (dict): The options for the docstring parser. See
-                                  parsers under griffe.docstrings.
-        docstring_section_style (str): The style used to render docstring sections.
-                                       Options: table, list, spacy. Default: "table".
-        merge_init_into_class (bool): Whether to merge the __init__ method into
-                                      the class' signature and docstring.
-                                      Default: False.
-        show_if_no_docstring (bool): Show the object heading even if it has no
-                                     docstring or children with docstrings.
-                                     Default: False.
-        show_docstring_attributes (bool): Whether to display the "Attributes"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_description (bool): Whether to display the textual block
-                                           (including admonitions) in the object's
-                                           docstring. Default: True.
-        show_docstring_examples (bool): Whether to display the "Examples"
-                                        section in the object's docstring.
-                                        Default: True.
-        show_docstring_other_parameters (bool): Whether to display the
-                                                "Other Parameters" section in the
-                                                object's docstring. Default: True.
-        show_docstring_parameters (bool): Whether to display the "Parameters"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_raises (bool): Whether to display the "Raises"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_receives (bool): Whether to display the "Receives"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_returns (bool): Whether to display the "Returns"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_warns (bool): Whether to display the "Warns"
-                                          section in the object's docstring.
-                                          Default: True.
-        show_docstring_yields (bool): Whether to display the "Yields"
-                                          section in the object's docstring.
-                                          Default: True.
-        Signatures/annotations options:
-
-        annotations_path (str): The verbosity for annotations path: brief
-                                (recommended), or source (as written in the source).
-                                Default: "brief".
-        line_length (int): Maximum line length when formatting code/signatures.
-                           Default: 60.
-        show_signature (bool): Show methods and functions signatures. Default: True.
-        show_signature_annotations (bool): Show the type annotations in methods
-                                           and functions signatures. Default: False.
-        signature_crossrefs (bool): Whether to render cross-references for type
-                                    annotations in signatures. Default: False.
-        separate_signature (bool): Whether to put the whole signature in a code
-                                  block below the heading. If Black is installed,
-                                  the signature is also formatted using it.
-                                  Default: False.
+        Arguments:
+            obj: What to show DocStrings for.
+            for_topmost: If True, try to find the "shortest" path to given object by
+                         checking whether it can also be found in a parent module.
+            allow_inspection: Whether to allow inspecting modules when visiting
+                              them is not possible
+            show_bases: Show the base classes of a class.
+            show_source: Show the source code of this object.
+            preload_modules: List of modules to pre-load.
+            heading_level: The initial heading level to use.
+            show_root_heading: Show the heading of the object at the root of the
+                               documentation tree (i.e. the object referenced by
+                               the identifier after :::).
+            show_root_toc_entry: If the root heading is not shown, at least
+                                 add a ToC entry for it.
+            show_root_full_path: Show the full Python path for the root
+                                 object heading.
+            show_root_members_full_path: Show the full Python path of the
+                                         root members.
+            show_object_full_path: Show the full Python path of every object.
+            show_category_heading: When grouped by categories, show a heading
+                                   for each category.
+            show_symbol_type_heading: Show the symbol type in headings (e.g. mod,
+                                      class, func and attr).
+            show_symbol_type_toc: Show the symbol type in the Table of
+                                  Contents (e.g. mod, class, func and attr).
+            members: An explicit list of members to render.
+            members_order: The members ordering to use.
+            filters: A list of filters applied to filter objects based on their name.
+                     A filter starting with ! will exclude matching objects instead of
+                     including them. The members option takes precedence over filters
+                     (filters will still be applied recursively to lower members in the
+                     hierarchy).
+            group_by_category: Group the object's children by categories:
+                               attributes, classes, functions, and modules.
+            show_submodules: When rendering a module, show its submodules recursively.
+            docstring_section_style: The style used to render docstring sections.
+            merge_init_into_class: Whether to merge the __init__ method into
+                                   the class' signature and docstring.
+            show_if_no_docstring: Show the object heading even if it has no
+                                  docstring or children with docstrings.
+            annotations_path: The verbosity for annotations path
+            line_length: Maximum line length when formatting code/signatures.
+            show_signature: Show methods and functions signatures.
+            show_signature_annotations: Show the type annotations in methods
+                                        and functions signatures.
+            signature_crossrefs: Whether to render cross-references for type
+                                 annotations in signatures.
+            separate_signature: Whether to put the whole signature in a code
+                                block below the heading. If Black is installed,
+                                the signature is also formatted using it.
+            kwargs: Keyword arguments passed to super.
         """
-        super().__init__(header=header)
+        super().__init__(**kwargs)
         self.obj = obj
         match obj:
             case types.ModuleType():
@@ -152,7 +126,35 @@ class DocStrings(markdownnode.Text):
                 self.module_path = mod.__name__
             case _:
                 raise TypeError(obj)
-        self.options = kwargs
+        self.options = self.OPTIONS_DEFAULT.copy()
+        self.options["allow_inspection"] = allow_inspection
+        self.options["show_bases"] = show_bases
+        self.options["show_source"] = show_source
+        self.options["preload_modules"] = preload_modules
+        self.options["heading_level"] = heading_level
+        self.options["show_root_heading"] = show_root_heading
+        self.options["show_root_toc_entry"] = show_root_toc_entry
+        self.options["show_root_full_path"] = show_root_full_path
+        self.options["show_root_members_full_path"] = show_root_members_full_path
+        self.options["show_object_full_path"] = show_object_full_path
+        self.options["show_category_heading"] = show_category_heading
+        self.options["show_symbol_type_heading"] = show_symbol_type_heading
+        self.options["show_symbol_type_toc"] = show_symbol_type_toc
+        self.options["members"] = members
+        self.options["members_order"] = members_order
+        self.options["filters"] = filters
+        self.options["group_by_category"] = group_by_category
+        self.options["show_submodules"] = show_submodules
+        self.options["docstring_section_style"] = docstring_section_style
+        self.options["merge_init_into_class"] = merge_init_into_class
+        self.options["show_if_no_docstring"] = show_if_no_docstring
+        self.options["annotations_path"] = annotations_path
+        self.options["line_length"] = line_length
+        self.options["show_signature"] = show_signature
+        self.options["show_signature_annotations"] = show_signature_annotations
+        self.options["signature_crossrefs"] = signature_crossrefs
+        self.options["separate_signature"] = separate_signature
+        self.options = {k: v for k, v in self.options.items() if v is not None}
 
     def __repr__(self):
         return utils.get_repr(self, obj=self.obj)
@@ -160,11 +162,11 @@ class DocStrings(markdownnode.Text):
     def _to_markdown(self) -> str:
         md = f"::: {self.module_path}\n"
         if self.options:
-            options = "\n".join(f"      {k}: {v}" for k, v in self.options.items())
+            options = "\n".join(f"      {k}: {v!r}" for k, v in self.options.items())
             md = f"{md}    options:\n{options}\n"
         return md
 
 
 if __name__ == "__main__":
-    docstrings = DocStrings("a.b", testus=10)
+    docstrings = DocStrings("a.b", show_submodules=True)
     print(docstrings)
