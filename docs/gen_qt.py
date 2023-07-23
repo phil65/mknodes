a4c24f3cb9e1caf1418350d8dc40e6685cb75ecf
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-23 22:31:56 +0200

    chore: cleanup

--- docs/gen_qt.py
+++ docs/gen_qt.py
@@ -10,9 +10,8 @@ prettyqt.import_all()
 QT_MODULE_ATTR = "QT_MODULE"
 
 root_nav = mknodes.MkNav()
-page = mknodes.MkPage(path="index.md", hide_toc=True, hide_nav=True)
+page = root_nav.add_index_page(hide_toc=True, hide_nav=True)
 page.add_header("Not in the mood to write documentation? Let´s code it then!", level=3)
-page.write()
 
 qt_docs = root_nav.add_documentation(prettyqt, section_name="Qt modules")
 extra_docs = root_nav.add_documentation(prettyqt, section_name="Additional modules")
