28678ff3fd5a2a50ee80e941a62fd5f35e42466a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-24 15:36:34 +0200

    chore: test work

--- tests/test_nav.py
+++ tests/test_nav.py
@@ -5,11 +5,11 @@ import mknodes
 
 def test_nav():
     nav = mknodes.MkNav()
-    # nav_file = pathlib.Path(__file__).parent / "SUMMARY.md"
-    # # print(pathlib.Path(nav_file).read_text())
-    # nav = MkNav.from_file(pathlib.Path(__file__).parent / "SUMMARY.md", None)
-    # lines = [f"{level * '    '} {node!r}" for level, node in nav.iter_nodes()]
-    # print("\n".join(lines))
+    nav.add_doc(mknodes)
+    nav.add_page("Test")
+    nav.add_index_page()
+    nav.add_nav("sub")
+
 
 def test_from_file(test_data_dir):
     nav = mknodes.MkNav.from_folder(test_data_dir / "nav_tree")
