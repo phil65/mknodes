4839e4e8021c5e9825d56bf58befbe417c793f81
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-26 17:54:36 +0200

    chore: add serve_script fn for debugging

---
+++ mknodes/utils/debugging.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import io
+import os
+import pathlib
+
+from mkdocs.commands import serve
+import yaml
+
+
+def serve_script(script_file: str | os.PathLike):
+    script_file = pathlib.Path(script_file).absolute()
+    script_file = script_file.relative_to(pathlib.Path.cwd())
+    text = pathlib.Path("mkdocs.yml").read_text()
+    config_file = yaml.load(text, Loader=yaml.Loader)
+    for plugin in config_file["plugins"]:
+        if isinstance(plugin, dict) and next(iter(plugin.keys())) == "gen-files":
+            plugin["gen-files"]["scripts"] = [script_file.as_posix()]
+    output = yaml.dump(config_file, Dumper=yaml.Dumper)
+    stream = io.StringIO(output)
+    serve.serve(
+        config_file=stream,
+        dev_addr=None,
+        livereload="livereload",
+        build_type=None,
+        watch_theme=False,
+        watch=[],
+    )
+
+
+if __name__ == "__main__":
+    serve_script("docs\\gen_qt.py")
