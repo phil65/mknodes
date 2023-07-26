22240dc5255daa7a7b98832150b319778289dc10
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-26 18:54:35 +0200

    chore: use annotations for manual

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -1,6 +1,6 @@
 """Generate the code reference pages and navigation."""
 
-from __future__ import annotations
+from __future__ import annotations as _annotations
 
 import pathlib
 
@@ -15,9 +15,11 @@ and without having to care about file paths at at all.
 Now check out what we have created!
 """
 
+annotations = mknodes.MkAnnotations()
+annotations[1] = "We will use annotations to explain things a bit."  # (1)
+
 # this Nav object is basically the root of everything. It corresponds to your root
 # SUMMARY.md.
-
 root_nav = mknodes.MkNav()
 
 # Let's begin with the start page. This is your index.md file.
@@ -25,10 +27,12 @@ page = root_nav.add_index_page(hide_toc=True, hide_nav=True)
 
 # The next 6 lines are generating the page you are looking at right now.
 page.add_header(HEADER, level=3)
-page.add_admonition(WARNING, typ="danger", title="Warning!")
+admonition = page.add_admonition(WARNING, typ="danger", title="Warning!")
+annotations[2] = admonition  # (2)
 page += "This is the source code for building this website:"
 code = pathlib.Path(__file__).read_text()
 page.add_code(code)
+page += annotations
 page += FOOTER
 
 # now we will create the nav section and its pages one by one.
