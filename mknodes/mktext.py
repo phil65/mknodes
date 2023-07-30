e29a6771e1003ff606a6fd10b94d36deadb7580d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-30 22:49:21 +0200

    feat: add getitem for MkText

--- mknodes/mktext.py
+++ mknodes/mktext.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 import logging
+import re
+
+from typing_extensions import Self
 
 from mknodes import mknode
 from mknodes.utils import helpers
@@ -27,6 +30,21 @@ class MkText(mknode.MkNode):
 
     def __repr__(self):
         return helpers.get_repr(self, text=self.text)
+
+    def __getitem__(self, section_name: str) -> Self | None:
+        markdown = self._to_markdown()
+        header_pattern = re.compile(f"^(#+) {section_name}$", re.MULTILINE)
+        header_match = header_pattern.search(markdown)
+        if header_match is None:
+            return None
+        section_level = len(header_match[1])
+        start_index = header_match.span()[1] + 1
+        end_pattern = re.compile(f"^#{{2,{section_level}}} ", re.MULTILINE)
+        end_match = end_pattern.search(markdown[start_index:])
+        if end_match is None:
+            return type(self)(markdown[start_index:])
+        end_index = end_match.span()[0]
+        return type(self)(markdown[start_index : end_index + start_index])
 
     def _to_markdown(self) -> str:
         return self.text if isinstance(self.text, str) else self.text.to_markdown()
--- tests/test_text.py
+++ tests/test_text.py
@@ -3,6 +3,16 @@ from __future__ import annotations
 import mknodes
 
 
-def test_text():
+def test_empty():
     nav = mknodes.MkText()
     assert not str(nav)
+
+
+def test_getitem_ending_with_eof():
+    nav = mknodes.MkText("## Test section\nTest")
+    assert str(nav["Test section"]) == "Test"
+
+
+def test_getitem_ending_with_another_section():
+    nav = mknodes.MkText("## Test section\nTest\n## Another section")
+    assert str(nav["Test section"]) == "Test\n"
