efdeb9b0ef7a73fd71eab1cb12b106b69320708d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-30 23:45:01 +0200

    feat(MkBlock): allow nesting blocks

--- mknodes/mkblock.py
+++ mknodes/mkblock.py
@@ -2,38 +2,53 @@ from __future__ import annotations
 
 import logging
 
-from mknodes import mknode
+from mknodes import mkcontainer, mknode, mktext
 
 
 logger = logging.getLogger(__name__)
 
 
-class MkBlock(mknode.MkNode):
+class MkBlock(mkcontainer.MkContainer):
     """pymdownx-based block."""
 
     def __init__(
         self,
         name: str,
-        content: str | mknode.MkNode = "",
+        content: list | str | mknode.MkNode = "",
         *,
         argument: str = "",
         attributes: dict[str, str | bool] | None = None,
         **kwargs,
     ):
-        super().__init__(**kwargs)
+        match content:
+            case None:
+                items: list[mknode.MkNode] = []
+            case str():
+                items = [mktext.MkText(content)]
+            case mknode.MkNode():
+                items = [content]
+            case list():
+                items = content
+            case _:
+                raise TypeError(content)
+        super().__init__(items=items, **kwargs)
         self.name = name
         self.attributes = attributes or {}
         self.argument = argument
-        self.content = content
+
+    @property
+    def content(self):
+        return self.items[0]
 
     def _to_markdown(self) -> str:
-        block_limiter = "///"
+        block_level = sum(isinstance(i, MkBlock) for i in self.ancestors)
+        block_limiter = "/" * (block_level + 3)
         base = f"{block_limiter} {self.name}"
         if self.argument:
             base += f" | {self.argument}"
         lines = [base]
         lines.extend(f"    {k}: {v}" for k, v in self.attributes.items())
-        lines.extend((str(self.content).rstrip("\n"), block_limiter))
+        lines.extend((super()._to_markdown().rstrip("\n"), block_limiter))
         return "\n".join(lines) + "\n"
 
     @staticmethod
@@ -46,5 +61,7 @@ class MkBlock(mknode.MkNode):
 
 
 if __name__ == "__main__":
-    test = MkBlock("tab", argument="abc", content="bcd", attributes=dict(new=True))
-    print(test)
+    inner_1 = MkBlock("inner_1", content="inner_1 content")
+    inner_2 = MkBlock("inner_2", content="inner_2 content")
+    outer = MkBlock("outer", content=[inner_1, inner_2])
+    print(outer)
