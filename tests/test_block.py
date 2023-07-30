f2333316ea099933e7c34372b4b757c06258146b
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-30 23:46:33 +0200

    test: add test for nested MkBlocks

--- tests/test_block.py
+++ tests/test_block.py
@@ -9,6 +9,18 @@ bcd
 ///
 """
 
+NESTED_EXPECTED = """/// outer
+//// inner_1
+inner_1 content
+////
+
+
+//// inner_2
+inner_2 content
+////
+///
+"""
+
 
 def test_block():
     node = mknodes.MkBlock(
@@ -18,3 +30,10 @@ def test_block():
         attributes=dict(new=True),
     )
     assert str(node) == EXPECTED
+
+
+def test_nested_block():
+    inner_1 = mknodes.MkBlock("inner_1", content="inner_1 content")
+    inner_2 = mknodes.MkBlock("inner_2", content="inner_2 content")
+    outer = mknodes.MkBlock("outer", content=[inner_1, inner_2])
+    assert str(outer) == NESTED_EXPECTED
