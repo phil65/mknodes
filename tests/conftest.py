79a53906f10f21e7de074ba63bd541d681afe871
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-31 12:56:41 +0200

    tests: resources_dir fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,8 @@ import pytest
 @pytest.fixture(scope="session")
 def test_data_dir():
     return pathlib.Path(__file__).parent / "data"
+
+
+@pytest.fixture(scope="session")
+def resources_dir():
+    return pathlib.Path(__file__).parent.parent / "mknodes/resources/"
