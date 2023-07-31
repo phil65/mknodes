a8ae043b7280f748a3ee1c468faf990f7e2cce46
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-07-31 09:23:46 +0200

    chore: support multiple providers for MkInstallGuide

--- mknodes/mkinstallguide.py
+++ mknodes/mkinstallguide.py
@@ -7,13 +7,46 @@ from mknodes import mknode
 
 logger = logging.getLogger(__name__)
 
-TEXT = """The latest released version is available at the [Python
+PIP_TEXT = """The latest released version is available at the [Python
 package index](https://pypi.org/project/{project}).
 
 ```sh
 pip install {project}
 ```
 """
+# !!! warning
+#     This method modifies the Python environment in which you choose to install.
+# Consider instead using [pipx](#pipx) to avoid dependency conflicts.
+
+PIPX_TEXT = """[pipx](https://github.com/pypa/pipx)
+allows for the global installation of Python applications in isolated environments.
+
+```
+pipx install {project}
+```
+"""
+HOMEBREW_TEXT = """See the [formula](https://formulae.brew.sh/formula/{project})
+for more details.
+
+```
+brew install {project}
+```
+"""
+
+CONDA_TEXT = """See the [feedstock](https://github.com/conda-forge/{project}-feedstock)
+for more details.
+
+```
+conda install -c conda-forge {project}
+```
+"""
+
+PROVIDERS = dict(
+    pip=PIP_TEXT,
+    pipx=PIPX_TEXT,
+    Homebrew=HOMEBREW_TEXT,
+    Conda=CONDA_TEXT,
+)
 
 
 class MkInstallGuide(mknode.MkNode):
@@ -22,13 +55,32 @@ class MkInstallGuide(mknode.MkNode):
     def __init__(
         self,
         project: str,
+        package_managers: list[str] | None = None,
+        header_level: int | None = 2,
         **kwargs,
     ):
+        """Constructor.
+
+        Arguments:
+            project: name of the project to install
+            package_managers: package managers the project can be installed with
+            header_level: Header level for each section
+            kwargs: Keyword arguments passed to parent
+        """
         super().__init__(**kwargs)
         self.project = project
+        self.header_level = header_level
+        self.package_managers = package_managers or ["pip"]
 
     def _to_markdown(self) -> str:
-        return TEXT.format(project=self.project)
+        blocks = []
+        for k, v in PROVIDERS.items():
+            if k in self.package_managers:
+                if self.header_level:
+                    prefix = self.header_level * "#"
+                    blocks.append(f"{prefix} {k}\n")
+                blocks.append(v.format(project=self.project))
+        return "\n\n".join(blocks)
 
     @staticmethod
     def create_example_page(page):
