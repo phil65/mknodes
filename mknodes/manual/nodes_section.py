284baeec8d8287997699695c47f00552ec879aec
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-02 03:37:40 +0200

    chore: cleanup

--- mknodes/manual/nodes_section.py
+++ mknodes/manual/nodes_section.py
@@ -20,17 +20,17 @@ def create_nodes_section(root_nav: mknodes.MkNav):
     # These markup nodes in some cases can contain other Markup nodes.
     # It`s all one big tree.
 
-    home_nav = root_nav.add_nav("The nodes")
-    overview = home_nav.add_index_page(hide_toc=True, icon="material/graph")
+    nodes_nav = root_nav.add_nav("The nodes")
+    overview = nodes_nav.add_index_page(hide_toc=True, icon="material/graph")
 
     # this here is what you are reading right now.
     overview += mknodes.MkCode.for_object(create_nodes_section, header=SECTION_CODE)
     overview += mknodes.MkDetailsBlock(INTRO_TEXT, expand=True)
-    create_nodes_subsection(home_nav)
-    create_subclass_page(home_nav)
+    create_nodes_subsection(nodes_nav)
+    create_subclass_page(nodes_nav)
 
 
-def create_nodes_subsection(nav):
+def create_nodes_subsection(nav: mknodes.MkNav):
     nodes_nav = nav.add_nav("Nodes")
     # let`s take a look at some of the mentioned Markup nodes.
     # we will now iter all node classes, create a small page (which is part of the menu)
@@ -43,12 +43,12 @@ def create_nodes_subsection(nav):
             create_class_page(kls, page)
             link = mknodes.MkLink(page, kls.__name__)
             table.add_row((link, kls.__doc__))
-    page = nodes_nav.add_index_page()
+    page = nodes_nav.add_index_page(hide_toc=True)
     page += mknodes.MkCode.for_object(create_nodes_subsection, header=SECTION_CODE)
     page += table
 
 
-def create_class_page(kls, page):
+def create_class_page(kls: type, page: mknodes.MkPage):
     # Each example page will begin by displaying the code used to create the page.
     page += mknodes.MkCode.for_object(
         create_class_page,
