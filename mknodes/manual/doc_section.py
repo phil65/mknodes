b7db022bde87b52e8a14106fb5071149d3dfef64
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-02 04:40:13 +0200

    docs: add Processor example

--- mknodes/manual/doc_section.py
+++ mknodes/manual/doc_section.py
@@ -1,5 +1,7 @@
 import mknodes
 
+from mknodes.templatenodes import processors
+
 
 INTRO_TEXT = """Now lets create the documentation.
 This code will show how to build a simple documentation section.
@@ -17,14 +19,7 @@ def create_documentation_section(root_nav: mknodes.MkNav):
         header=SECTION_CODE,
     )
     overview += mknodes.MkAdmonition(INTRO_TEXT, typ="tip")
-
-    # lets create the complete documentation for our module.
-    # Each Documentation section can have global filters for what it should include.
-    # In this case, we only want to document stuff which is listed in "__all__".
-    mknodes_docs = doc_section.add_doc(module=mknodes, filter_by___all__=True)
-
-    # now we collect the stuff we want to document.
-    mknodes_docs.collect_classes(recursive=True)
+    create_mknodes_section(doc_section)
 
     # We could also filter specific subclasses,
     # or do other fancy stuff to generate a customized, automated documentation
@@ -38,3 +33,54 @@ def create_documentation_section(root_nav: mknodes.MkNav):
 
     # addon_docs = doc_section.add_doc(module=prettyqtmarkdown, flatten_nav=True)
     # addon_docs.collect_classes(recursive=True)
+
+
+def create_mknodes_section(nav: mknodes.MkNav):
+    # lets create the documentation for our module.
+    # For that, we can use the MkDoc node, which will generate docs for us.
+    # Usually, this can be done with 2 or 3 lines of code, but
+    # since our aim is to always show the code which generated the site, we will have to
+    # do some extra steps and adjust the default page template.
+    # So lets subclass MkClassPage and extend it.
+    # First, we write a custom processor which fetches the page-building code from the
+    # existing processors, puts them into code blocks and adds them to the page.
+
+    class SourceCodeProcessor(processors.PageProcessor):
+        def __init__(
+            self,
+            *args,
+            processors: list[processors.PageProcessor] | None = None,
+            **kwargs,
+        ):
+            super().__init__(*args, **kwargs)
+            self.processors = processors or []
+
+        def append_block(self, page: mknodes.MkPage):
+            for processor in self.processors:
+                # First, we check if the processor gets applied.
+                # If yes, we attach a code block.
+                if processor.check_if_apply(page):
+                    page += mknodes.MkCode.for_object(processor.append_block)
+
+        def get_header(self, page):
+            return "Code for this page"
+
+    # Now, we write a custom page template which
+    # overrides get_processors and includes our new processor at the beginning.
+
+    class CustomClassPage(mknodes.MkClassPage):
+        def get_processors(self):
+            processors = super().get_processors()
+            code_processor = SourceCodeProcessor(self.klass, processors=processors)
+            return [code_processor, *processors]
+
+    # Now that we have our custom ClassPage, we can create the documentation.
+    # In our case, we only want to document stuff which is listed in "__all__".
+    mknodes_docs = nav.add_doc(
+        module=mknodes,
+        filter_by___all__=True,
+        class_page=CustomClassPage,
+    )
+
+    # now we collect the stuff we want to document.
+    mknodes_docs.collect_classes(recursive=True)
