b4b3b61d8927a26c63bee809f32d441b20064b85
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-03 11:49:21 +0200

    chore: cleanup index page

--- mknodes/manual/index_page.py
+++ mknodes/manual/index_page.py
@@ -3,24 +3,23 @@ import mknodes
 from mknodes.basenodes import mkadmonition
 
 
+OPEN_TEXT = "Open me to see the markup!"
+INNER_TEXT = "You did it!"
+
+
 def create_index_page(nav: mknodes.MkNav):
     page = nav.add_page("Fancy index page", hide_toc=True, hide_nav=True)
     page += mknodes.MkShields(
-        shields=["version", "status", "codecov"],
+        ["version", "status", "codecov"],
         user="phil65",
         project="mknodes",
     )
-    code = mknodes.MkCode.for_object(create_index_page)
-    node = mknodes.MkAdmonition(content=code, title="")
+    fn_code = mknodes.MkCode.for_object(create_index_page)
+    node = mknodes.MkAdmonition(content=fn_code, title="")
+    page_markdown = mknodes.MkCode(str(page))
+    md_node = mknodes.MkAdmonition(content=page_markdown, title=INNER_TEXT)
     for i in mkadmonition.AdmonitionTypeStr.__args__:
         node = mknodes.MkAdmonition(content=node, typ=i, title="")
+        md_node = mknodes.MkAdmonition(md_node, typ=i, collapsible=True, title=OPEN_TEXT)
     page += node
-    node = mknodes.MkAdmonition(content=mknodes.MkCode(str(page)), title="You did it!")
-    for i in mkadmonition.AdmonitionTypeStr.__args__:
-        node = mknodes.MkAdmonition(
-            content=node,
-            typ=i,
-            collapsible=True,
-            title="Open me to see the markup!",
-        )
-    page += node
+    page += md_node
