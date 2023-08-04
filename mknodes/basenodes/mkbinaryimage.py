b45a775d576279c06bcfe66727f06cc198b3287b
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-04 08:01:40 +0200

    chore: improve MkBinaryImage data

--- mknodes/basenodes/mkbinaryimage.py
+++ mknodes/basenodes/mkbinaryimage.py
@@ -40,9 +40,9 @@ class MkBinaryImage(mkimage.MkImage):
 
         page += "A BinaryImage carries the image data by itself."
         page += "A file containing the data will become part of the file tree later on."
-        data = """<svg width="45pt" height="30pt" version="1.1"
+        data = """<svg width="90pt" height="90pt" version="1.1"
         viewBox="0 0 15.875 10.583" xmlns="http://www.w3.org/2000/svg">
-         <g fill="none" stroke="#000" stroke-width=".17639">
+         <g fill="none" stroke="#F00" stroke-width=".3">
           <path d="m6.1295 3.6601 3.2632 3.2632z"/>
           <path d="m9.3927 3.6601-3.2632 3.2632z"/>
          </g>
