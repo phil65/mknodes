adb237ff42e713c760d1be8fbb2e0565297c98ac
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-06 00:54:56 +0200

    test: add test for docstrings inside annotations

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -3,6 +3,18 @@ from __future__ import annotations
 import mknodes
 
 
+EXPECTED_IN_ANNOTATIONS = """1.  ::: mknodes.basenodes.mkdocstrings.MkDocStrings.__init__
+        options:
+          docstring_section_style: 'list'
+    """ + "\n"  # noqa: ISC003
+
+
 def test_docstrings():
     docstrings = mknodes.MkDocStrings(obj=mknodes)
     assert str(docstrings) == "::: mknodes\n"
+
+
+def test_auto_list_style_inside_annotations():
+    annotations = mknodes.MkAnnotations()
+    annotations[1] = mknodes.MkDocStrings(obj=mknodes.MkDocStrings.__init__)
+    assert str(annotations) == EXPECTED_IN_ANNOTATIONS
