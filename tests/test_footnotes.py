9b31f0230dd194f95b641932a624b5811cd2146b
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-05 09:06:37 +0200

    test: add tests for MkAnnotations / MkFootNotes

--- tests/test_footnotes.py
+++ tests/test_footnotes.py
@@ -5,58 +5,70 @@ import mknodes
 
 EXPECTED = """## Header
 
-1.  abcde
+[^1]:
+    abcde
     fghi
-2.  abcde
+[^2]:
+    abcde
     fghi
-3.  abcde
+[^3]:
+    abcde
     fghi
-4.  abcde
+[^4]:
+    abcde
     fghi
-5.  abcde
+[^5]:
+    abcde
     fghi
-6.  abcde
+[^6]:
+    abcde
     fghi
-7.  abcde
+[^7]:
+    abcde
     fghi
-8.  abcde
+[^8]:
+    abcde
     fghi
-9.  abcde
+[^9]:
+    abcde
     fghi
-10. abcde
+[^10]:
+    abcde
     fghi
 """
 
-EXPECTED_SORTED = """1.  1
-2.  2
+EXPECTED_SORTED = """[^1]:
+    1
+[^2]:
+    2
 """
 
 
 def test_empty():
-    annotation = mknodes.MkAnnotations()
+    annotation = mknodes.MkFootNotes()
     assert not str(annotation)
 
 
 def test_if_annotations_get_sorted():
-    node = mknodes.MkAnnotations()
+    node = mknodes.MkFootNotes()
     node[2] = "2"
     node[1] = "1"
     assert str(node) == EXPECTED_SORTED
 
 
 def test_markdown():
-    annotation = mknodes.MkAnnotations(["abcde\nfghi"] * 10, header="Header")
+    annotation = mknodes.MkFootNotes(["abcde\nfghi"] * 10, header="Header")
     assert str(annotation) == EXPECTED
 
 
 def test_constructors():
-    annotation_1 = mknodes.MkAnnotations(["abc", "def"])
+    annotation_1 = mknodes.MkFootNotes(["abc", "def"])
     anns = {1: "abc", 2: "def"}
-    annotation_2 = mknodes.MkAnnotations(anns)
+    annotation_2 = mknodes.MkFootNotes(anns)
     assert str(annotation_1) == str(annotation_2)
 
 
 def test_mapping_interface():
-    ann = mknodes.MkAnnotations()
+    ann = mknodes.MkFootNotes()
     ann[1] = "test"
-    assert str(ann[1]) == "1.  test\n"
+    assert str(ann[1]) == "[^1]:\n    test\n"
