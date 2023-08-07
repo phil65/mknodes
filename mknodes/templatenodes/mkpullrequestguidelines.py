3ffd98c0771c08a8fcf6ae9c666b8d6a9c585d85
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-07 11:27:20 +0200

    refactor: make MkPullRequestGuideLines a container

--- mknodes/templatenodes/mkpullrequestguidelines.py
+++ mknodes/templatenodes/mkpullrequestguidelines.py
@@ -4,39 +4,24 @@ import logging
 
 from typing import Any
 
-from mknodes.basenodes import mknode
+from mknodes.basenodes import mkcode, mkcontainer, mktext
 
 
 logger = logging.getLogger(__name__)
 
 
-TEXT = """Link to any related issue in the Pull Request message.
+INTRO = """Link to any related issue in the Pull Request message.
 
-During the review, we recommend using fixups:
+During the review, we recommend using fixups:"""
 
-```bash
-# SHA is the SHA of the commit you want to fix
-git commit --fixup=SHA
-```
+SQUASH_TEXT = "Once all the changes are approved, you can squash your commits:"
 
-Once all the changes are approved, you can squash your commits:
-
-```bash
-git rebase -i --autosquash main
-```
-
-And force-push:
-
-```bash
-git push -f
-```
-
-If this seems all too complicated, you can push or force-push each new commit,
+OUTRO = """If this seems all too complicated, you can push or force-push each new commit,
 and we will squash them ourselves if needed, before merging.
 """
 
 
-class MkPullRequestGuidelines(mknode.MkNode):
+class MkPullRequestGuidelines(mkcontainer.MkContainer):
     """Pull request guide text."""
 
     ICON = "octicons/git-pull-request-24"
@@ -54,8 +39,29 @@ class MkPullRequestGuidelines(mknode.MkNode):
         """
         super().__init__(header=header, **kwargs)
 
-    def _to_markdown(self) -> str:
-        return TEXT
+    @property
+    def items(self):
+        return [
+            mktext.MkText(INTRO, parent=self),
+            mkcode.MkCode(
+                "git commit --fixup=SHA # SHA of commit you want to fix",
+                language="bash",
+                parent=self,
+            ),
+            mktext.MkText(SQUASH_TEXT, parent=self),
+            mkcode.MkCode(
+                "git rebase -i --autosquash main",
+                language="bash",
+                parent=self,
+            ),
+            mktext.MkText("And force-push:", parent=self),
+            mkcode.MkCode("git push -f", language="bash", parent=self),
+            mktext.MkText(OUTRO, parent=self),
+        ]
+
+    @items.setter
+    def items(self, value):
+        pass
 
     @staticmethod
     def create_example_page(page):
