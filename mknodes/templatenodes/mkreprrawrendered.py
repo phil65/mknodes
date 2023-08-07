92295c9929c4deecff24b3fc2edad4fc58df317f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-07 13:37:59 +0200

    chore: rework ReprRawRendered

--- mknodes/templatenodes/mkreprrawrendered.py
+++ mknodes/templatenodes/mkreprrawrendered.py
@@ -4,20 +4,12 @@ import logging
 
 from typing import Any
 
-from mknodes.basenodes import mkcode, mknode, mktabcontainer
+from mknodes.basenodes import mkcode, mknode, mktabcontainer, mktabs
 from mknodes.pages import mkpage
 from mknodes.utils import helpers
 
 
 logger = logging.getLogger(__name__)
-
-
-def prep(node):
-    node = node.__copy__()
-    if isinstance(node, mkpage.MkPage):
-        node.parent = None
-        node.path = f"__{node.path}"
-    return node
 
 
 class MkReprRawRendered(mktabcontainer.MkTabbed):
@@ -32,29 +24,41 @@ class MkReprRawRendered(mktabcontainer.MkTabbed):
             node: Node to show an example for
             kwargs: Keyword arguments passed to parent
         """
-        repr_node = mkcode.MkCode(repr(node))
-        if len(node.children) > 0:
-            lines = [f"{level * '    '} {node!r}" for level, node in node.iter_nodes()]
-            tree = mkcode.MkCode("\n".join(lines))
-        else:
-            tree = None
-        markdown_node = mkcode.MkCode(prep(node), language="markdown")
+        self.node = node
+        super().__init__(tabs={}, select_tab=2, **kwargs)
+
+    def __repr__(self):
+        return helpers.get_repr(self, node=self.node)
+
+    @property
+    def items(self):
         # TODO: hack: without doing this, we get issues because the page becomes
         # part of the tree. Perhaps add a setting for MkPages to be only-virtual?
         # Needs a general concept in regards to re-parenting. (should base nodes
         # be allowed to have pages as children?)
-        self.node = node
         tabs: dict[str, str | mknode.MkNode] = dict(
-            Repr=repr_node,
-            Markdown=markdown_node,
-            Rendered=prep(node),
+            Repr=mkcode.MkCode(repr(self.node)),
+            Markdown=mkcode.MkCode(self.prep(self.node), language="markdown"),
+            Rendered=str(self.node),
         )
-        if tree:
-            tabs["Repr tree"] = tree
-        super().__init__(tabs=tabs, select_tab=2, **kwargs)
+        if len(self.node.children) > 0:
+            lines = [f"{level * '    '} {n!r}" for level, n in self.node.iter_nodes()]
+            tabs["Repr tree"] = mkcode.MkCode("\n".join(lines))
+        items = [mktabs.MkTab(k, v, parent=self) for k, v in tabs.items()]
+        items[0].new = True
+        return items
 
-    def __repr__(self):
-        return helpers.get_repr(self, node=self.node)
+    @items.setter
+    def items(self, value):
+        pass
+
+    def prep(self, node):
+        # node.parent = self
+        if isinstance(node, mkpage.MkPage):
+            node = node.__copy__()
+            node.parent = None
+            node.path = f"__{node.path}"
+        return node
 
     @staticmethod
     def create_example_page(page):
