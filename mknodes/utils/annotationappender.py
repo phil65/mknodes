2ab015b48475bc2bf659965968ce59944d09fbe0
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-08 01:05:59 +0200

    feat: add AnnotationAppender

---
+++ mknodes/utils/annotationappender.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import re
+
+
+CLASS_REGEX = re.compile(r"\b((mknodes\.Mk[A-Za-z\.\_]*).*)")
+
+
+class AnnotationAppender:
+    """Helper for auto-annotating code blocks.
+
+    Example:
+        test = mknodes.MkAdmonition
+
+        mknodes.MkNode
+
+          mknodes.MkAdmonition
+        "
+        appender = AnnotationAppender()
+        text = appender.append_markers(test)
+        appender.append_annotations(node)
+
+    """
+
+    def __init__(self):
+        self.count = 0
+        self.matches = []
+
+    def __call__(self, match):
+        line = match.group()
+        self.count += 1
+        self.matches.append((self.count, match.group(2)))
+        return f"{line} # ({self.count})"
+
+    def append_markers(self, code: str) -> str:
+        return re.sub(CLASS_REGEX, self, code) if "# (" not in code else code
+
+    def append_annotations(self, node):
+        import mknodes
+
+        for index, dotted_path in self.matches:
+            node[index] = mknodes.MkDocStrings(dotted_path)
