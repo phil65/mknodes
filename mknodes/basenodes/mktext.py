ad9a2322c76d969b31b1d385ec5b80bb24338296
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-08 08:29:41 +0200

    chore: cleanup

--- mknodes/basenodes/mktext.py
+++ mknodes/basenodes/mktext.py
@@ -18,6 +18,21 @@ RESPONSE_CODE_OK = 200
 EXAMPLE_URL = "https://raw.githubusercontent.com/phil65/mknodes/main/README.md"
 
 
+def extract_header_section(markdown: str, section_name: str) -> str | None:
+    header_pattern = re.compile(f"^(#+) {section_name}$", re.MULTILINE)
+    header_match = header_pattern.search(markdown)
+    if header_match is None:
+        return None
+    section_level = len(header_match[1])
+    start_index = header_match.span()[1] + 1
+    end_pattern = re.compile(f"^#{{2,{section_level}}} ", re.MULTILINE)
+    end_match = end_pattern.search(markdown[start_index:])
+    if end_match is None:
+        return markdown[start_index:]
+    end_index = end_match.span()[0]
+    return markdown[start_index : end_index + start_index]
+
+
 class MkText(mknode.MkNode):
     """Class for any Markup text.
 
@@ -48,18 +63,8 @@ class MkText(mknode.MkNode):
 
     def __getitem__(self, section_name: str) -> Self | None:
         markdown = self._to_markdown()
-        header_pattern = re.compile(f"^(#+) {section_name}$", re.MULTILINE)
-        header_match = header_pattern.search(markdown)
-        if header_match is None:
-            return None
-        section_level = len(header_match[1])
-        start_index = header_match.span()[1] + 1
-        end_pattern = re.compile(f"^#{{2,{section_level}}} ", re.MULTILINE)
-        end_match = end_pattern.search(markdown[start_index:])
-        if end_match is None:
-            return type(self)(markdown[start_index:])
-        end_index = end_match.span()[0]
-        return type(self)(markdown[start_index : end_index + start_index])
+        section_text = extract_header_section(markdown, section_name)
+        return None if section_text is None else type(self)(section_text)
 
     def _to_markdown(self) -> str:
         return self.text if isinstance(self.text, str) else self.text.to_markdown()
