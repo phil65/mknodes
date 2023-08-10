312caf72621ff15d7903d15b8f2c018c9ff301ce
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-10 07:03:49 +0200

    chore: get rid of warnings

--- mknodes/config.py
+++ mknodes/config.py
@@ -3,15 +3,25 @@ from __future__ import annotations
 from mkdocs import config
 
 
-CONFIG = config.load_config("mkdocs.yml")
+CONFIG = None
+
+# Very dumb caching... Ideally we would be a proper plugin and get the config from
+# the callbacks
+
+
+def get_config():
+    global CONFIG
+    if CONFIG is None:
+        CONFIG = config.load_config("mkdocs.yml")
+    return CONFIG
 
 
 def get_repository_url():
-    return CONFIG.repo_url
+    return get_config().repo_url
 
 
 def get_site_url():
-    return CONFIG.site_url
+    return get_config().site_url
 
 
 if __name__ == "__main__":
