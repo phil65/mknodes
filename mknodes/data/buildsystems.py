07a96edd9dee3ebe85ef7b0758e44d6a10265182
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-10 14:58:22 +0200

    chore: bit more buildsystem info

--- mknodes/data/buildsystems.py
+++ mknodes/data/buildsystems.py
@@ -3,22 +3,50 @@ from __future__ import annotations
 import dataclasses
 
 
+RAW_GITHUB = "https://raw.githubusercontent.com"
+
+
 @dataclasses.dataclass
 class BuildSystem:
     identifier: str
     build_backend: str
     url: str
+    env_setup_cmd: str | None
+    install_url: str | None = None
 
 
-hatch = BuildSystem("hatch", "hatchling.build", "https://hatch.pypa.io")
-flit = BuildSystem("flit", "flit_core.buildapi", "https://flit.pypa.io")
-poetry = BuildSystem("poetry", "poetry.core.masonry.api", "https://python-poetry.org")
-setuptools = BuildSystem(
-    "setuptools",
-    "setuptools.build_meta",
-    "https://pypi.org/project/setuptools/",
+hatch = BuildSystem(
+    identifier="hatch",
+    build_backend="hatchling.build",
+    url="https://hatch.pypa.io",
+    env_setup_cmd="hatch env create",
+    install_url=f"{RAW_GITHUB}/pypa/hatch/master/docs/install.md",
 )
-pdm = BuildSystem("pdm", "pdm.backend", "https://pdm.fming.dev/")
+flit = BuildSystem(
+    identifier="flit",
+    build_backend="flit_core.buildapi",
+    url="https://flit.pypa.io",
+    env_setup_cmd="flit install",
+)
+poetry = BuildSystem(
+    identifier="poetry",
+    build_backend="poetry.core.masonry.api",
+    url="https://python-poetry.org",
+    env_setup_cmd="poetry install",
+)
+setuptools = BuildSystem(
+    identifier="setuptools",
+    build_backend="setuptools.build_meta",
+    url="https://pypi.org/project/setuptools/",
+    env_setup_cmd=None,
+)
+pdm = BuildSystem(
+    identifier="pdm",
+    build_backend="pdm.backend",
+    url="https://pdm.fming.dev/",
+    env_setup_cmd=None,
+    install_url=f"{RAW_GITHUB}/pdm-project/pdm/main/docs/docs/index.md#Installation",
+)
 
 BUILD_SYSTEMS: dict[str, BuildSystem] = {
     p.identifier: p for p in [hatch, flit, poetry, setuptools, pdm]
