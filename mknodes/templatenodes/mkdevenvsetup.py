369b1e5661982d251771a4fb433a41bd4f3713ce
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-10 19:28:50 +0200

    chore: cleanup

--- mknodes/templatenodes/mkdevenvsetup.py
+++ mknodes/templatenodes/mkdevenvsetup.py
@@ -4,7 +4,15 @@ import logging
 
 from typing import Any
 
-from mknodes.basenodes import mkadmonition, mkcode, mkcontainer, mklink, mktext
+from mknodes.basenodes import (
+    mkadmonition,
+    mkcode,
+    mkcontainer,
+    mkheader,
+    mklink,
+    mknode,
+    mktext,
+)
 from mknodes.data import buildsystems
 from mknodes.utils import helpers
 
@@ -26,6 +34,45 @@ PRE_COMMIT_CODE = """
 # Setup pre-commit hooks for required formatting
 pre-commit install
 """
+
+
+def get_docs_section(backend_name: str, docs_setup: str) -> list[mknode.MkNode]:
+    return [
+        mkheader.MkHeader("Docs Development"),
+        mktext.MkText(f"{backend_name} uses {docs_setup} to build the docs."),
+        mktext.MkText("To build the docs:"),
+        mkcode.MkCode("mkdocs build"),
+        mktext.MkText("To serve the docs locally at http://127.0.0.1:8000/:"),
+        mkcode.MkCode("mkdocs serve"),
+        mktext.MkText("For additional mkdocs help and options:"),
+        mkcode.MkCode("mkdocs --help"),
+    ]
+
+
+def get_build_backend_section(backend: buildsystems.BuildSystem) -> list[mknode.MkNode]:
+    backend_name = backend.identifier.capitalize()
+    return [
+        mktext.MkText(f"{backend_name} is used as the build system."),
+        mkcode.MkCode(f"pip install {backend.identifier}", language="bash"),
+        mklink.MkLink(backend.url, "More information"),
+    ]
+
+
+def get_pre_commit_section() -> list[mknode.MkNode]:
+    return [
+        mkheader.MkHeader("Pre-commit"),
+        mktext.MkText("This project uses `pre-commit` to ensure code quality."),
+        mkcode.MkCode(PRE_COMMIT_CODE, language="md"),
+        mkadmonition.MkAdmonition(
+            [
+                "To install pre-commit:",
+                mkcode.MkCode("pip install pre-commit", language="bash"),
+                mklink.MkLink("https://pre-commit.com", "More information"),
+            ],
+            collapsible=True,
+            title="Installing pre-commit",
+        ),
+    ]
 
 
 class MkDevEnvSetup(mkcontainer.MkContainer):
@@ -89,54 +136,22 @@ class MkDevEnvSetup(mkcontainer.MkContainer):
                 use_pre_commit = True
             case _:
                 raise TypeError(self.use_pre_commit)
-        items = [
-            mktext.MkText(START_TEXT, parent=self),
-            mkcode.MkCode(
-                CLONE_CODE.format(repo_url=repo_url, folder_name=folder_name),
-                language="md",
-                parent=self,
-            ),
-        ]
-        if use_pre_commit:
-            items.extend(
-                [
-                    mktext.MkText(
-                        "This project uses `pre-commit` to ensure code quality.",
-                        parent=self,
-                    ),
-                    mkcode.MkCode(PRE_COMMIT_CODE, language="md", parent=self),
-                    mkadmonition.MkAdmonition(
-                        [
-                            "To install pre-commit:",
-                            mkcode.MkCode("pip install pre-commit", language="bash"),
-                            mklink.MkLink("https://pre-commit.com", "More information"),
-                        ],
-                        collapsible=True,
-                        title="Installing pre-commit",
-                    ),
-                ],
-            )
         match self.build_backend:
             case str():
                 backend = buildsystems.BUILD_SYSTEMS[self.build_backend]
             case None if self.associated_project:
                 backend = self.associated_project.pyproject.build_system()
             case _:
-                return items
-        items.extend(
-            [
-                mktext.MkText(
-                    f"{backend.identifier.capitalize()} is used as the build system.",
-                    parent=self,
-                ),
-                mkcode.MkCode(
-                    f"pip install {backend.identifier}",
-                    language="bash",
-                    parent=self,
-                ),
-                mklink.MkLink(backend.url, "More information"),
-            ],
-        )
+                backend = buildsystems.setuptools
+        docs_setup = "MkDocs + Material theme"
+        code = CLONE_CODE.format(repo_url=repo_url, folder_name=folder_name)
+        items = [mktext.MkText(START_TEXT), mkcode.MkCode(code, language="md")]
+        if use_pre_commit:
+            items.extend(get_pre_commit_section())
+        items.extend(get_build_backend_section(backend))
+        items.extend(get_docs_section(docs_setup=docs_setup, backend_name=folder_name))
+        for item in items:
+            item.parent = self
         return items
 
     @items.setter
