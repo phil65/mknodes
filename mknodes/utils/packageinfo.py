8d25b59f6277f14be68e31c68884f100a37450fb
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-10 18:48:16 +0200

    chore: some love for PackageInfo

--- mknodes/utils/packageinfo.py
+++ mknodes/utils/packageinfo.py
@@ -101,7 +101,7 @@ class PackageInfo:
             if k == "Project-URL"
         }
         requires = get_requires(self.distribution)
-        self.requirements = [get_dependency(i) for i in requires] if requires else []
+        self.required_deps = [get_dependency(i) for i in requires] if requires else []
         self.classifiers = [v for h, v in self.metadata.items() if h == "Classifier"]
         self.version = self.metadata["Version"]
         self.metadata_version = self.metadata["Metadata-Version"]
@@ -114,6 +114,7 @@ class PackageInfo:
         return hash(self.package_name)
 
     def get_license(self) -> str:
+        """Get name of the license."""
         if license_name := self.metadata.get("License-Expression", "").strip():
             return license_name
         return next(
@@ -126,29 +127,54 @@ class PackageInfo:
         )
 
     def get_license_file_path(self) -> pathlib.Path | None:
+        """Return license file path (relative to project root) from metadata."""
         file = self.metadata.get("License-File")
         return pathlib.Path(file) if file else None
 
     def get_repository_url(self) -> str | None:
+        """Return repository URL from metadata."""
         if "Source" in self.urls:
             return self.urls["Source"]
         return self.urls["Repository"] if "Repository" in self.urls else None
 
     def get_repository_username(self) -> str | None:
+        """Try to extract repository username from metadata."""
         if match := GITHUB_REGEX.match(self.get_repository_url() or ""):
             return match.group(1)
         return None
 
     def get_repository_name(self) -> str | None:
+        """Try to extract repository name from metadata."""
         if match := GITHUB_REGEX.match(self.get_repository_url() or ""):
             return match.group(2)
         return None
 
     def get_keywords(self) -> list[str]:
+        """Return a list of keywords from metadata."""
         return self.metadata.get("Keywords", "").split(",")
 
+    def get_classifiers(self) -> dict[str, list[str]]:
+        """Return a dict containing the classifier categories and values from metadata.
+
+        {category_1: [classifier_1, ...],
+         category_2, [classifier_x, ...],
+         ...
+         }
+        """
+        classifiers: dict[str, list[str]] = {}
+        for k, v in self.metadata.items():
+            if k == "Classifier":
+                category = v.split(" :: ")[0]
+                value = v.split(" :: ", 1)[1]
+                if category in classifiers:
+                    classifiers[category].append(value)
+                else:
+                    classifiers[category] = [value]
+        return classifiers
+
     def get_required_package_names(self) -> list[str]:
-        return [i.name for i in self.requirements]
+        """Get a list of names from required packages."""
+        return [i.name for i in self.required_deps]
 
     def get_required_packages(self) -> dict[PackageInfo, Dependency]:
         modules = (
@@ -162,14 +188,22 @@ class PackageInfo:
                 packages[get_info(mod)] = self.get_dep_info(mod)
         return packages
 
-    def get_dep_info(self, name):
-        for i in self.requirements:
+    def get_dep_info(self, name: str) -> Dependency:
+        for i in self.required_deps:
             if i.name == name:
                 return i
         raise ValueError(name)
 
-    def get_extras(self) -> set[str]:
-        return {extra for dep in self.requirements for extra in dep.extras}
+    def get_extras(self) -> dict[str, list[str]]:
+        """Return a dict containing extras and the packages {extra: [package_1, ...]}."""
+        extras: dict[str, list[str]] = {}
+        for dep in self.required_deps:
+            for extra in dep.extras:
+                if extra in extras:
+                    extras[extra].append(dep.name)
+                else:
+                    extras[extra] = [dep.name]
+        return extras
 
     def get_author_email(self) -> str:
         mail = self.metadata["Author-Email"].split(" ")[-1]
@@ -181,4 +215,4 @@ class PackageInfo:
 
 if __name__ == "__main__":
     info = get_info("mknodes")
-    print(info.get_author_name())
+    print(info.get_extras())
