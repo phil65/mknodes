2739cb9edb22b273a3c28b24adaf49b3b53f8e63
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-11 11:47:09 +0200

    chore: linkreplacer cleanup

--- mknodes/linkreplacerplugin/linkreplacerplugin.py
+++ mknodes/linkreplacerplugin/linkreplacerplugin.py
@@ -44,33 +44,24 @@ class AutoLinkReplacerPlugin:
         self.linker_url = os.path.dirname(self.base_docs_url / page_url)  # noqa: PTH120
 
     def __call__(self, match):
-        # Name of the markdown file
         filename = urllib.parse.unquote(match.group(3).strip())
         if filename not in self.mapping:
             return f"`{match.group(3).replace('.md', '')}`"
         filenames = self.mapping[filename]
-        # if len(filenames) > 1:
-        #     logger.warning(
-        #         f"{self.page_url}: {match.group(3)} has multiple targets: {filenames}"
-        #     )
+        if len(filenames) > 1:
+            text = "%s: %s has multiple targets: %s"
+            logger.debug(text, self.page_url, match.group(3), filenames)
         abs_link_url = (self.base_docs_url / filenames[0]).parent
         # need os.replath here bc pathlib.relative_to throws an exception
         # when linking across drives
         rel_path = os.path.relpath(abs_link_url, self.linker_url)
-        # html_filename = filename.replace(".md", ".html")
         rel_link_url = os.path.join(rel_path, filename)  # noqa: PTH118
-        new_text = (
-            match.group(0).replace(match.group(2), rel_link_url)
-            if match.group(5) is None
-            else match.group(0).replace(match.group(2), rel_link_url + match.group(5))
-        )
+        new_text = match.group(0).replace(match.group(2), rel_link_url)
+        to_replace_with = rel_link_url + (match.group(5) or "")
+        new_text = match.group(0).replace(match.group(2), to_replace_with)
         new_text = new_text.replace("\\", "/")
-        logger.debug(
-            "LinkReplacer: %s: %s -> %s",
-            self.page_url,
-            match.group(3),
-            rel_link_url,
-        )
+        text = "LinkReplacer: %s: %s -> %s"
+        logger.debug(text, self.page_url, match.group(3), rel_link_url)
         return new_text
 
 
@@ -90,6 +81,5 @@ class LinkReplacerPlugin(BasePlugin):
             filename = os.path.basename(file_.abs_src_path)  # noqa: PTH119
             mapping[filename].append(file_.url)
         #     print(file_.url, file_.dest_uri)
-        # raise ValueError
         plugin = AutoLinkReplacerPlugin(base_docs_url, page_url, mapping)
         return re.sub(AUTOLINK_RE, plugin, markdown)
