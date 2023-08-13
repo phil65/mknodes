e2563af9441fb540e0d74696ad0a9ed973e41a9c
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-13 10:12:10 +0200

    chore: docstrings

--- mknodes/manual/__init__.py
+++ mknodes/manual/__init__.py
@@ -1,3 +1,5 @@
+"""Module containing the documentation."""
+
 from .nodes_section import create_nodes_section
 from .internals_section import create_internals_section
 from .dev_section import create_development_section
---
+++ mknodes/pages/__init__.py
@@ -0,0 +1,1 @@
+"""Module containing MkPages and its subclasses."""
