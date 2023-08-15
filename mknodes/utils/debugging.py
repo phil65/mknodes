27dfbb7bdfcece682607e604d295b0cddbb1892c
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-15 10:32:04 +0200

    chore: add serve_node helper

--- mknodes/utils/debugging.py
+++ mknodes/utils/debugging.py
@@ -7,20 +7,66 @@ import pathlib
 from mkdocs.commands import serve
 import yaml
 
-from mknodes import mkdocsconfig
+
+# from mknodes import mkdocsconfig
+
+
+content = """
+import mknodes
+
+def build(project):
+    project.module = mknodes
+    root = project.get_root()
+    page = root.add_index_page(hide_toc=True)
+    page += '''{node}'''
+
+
+"""
+
+
+def serve_node(node):
+    text = content.format(node=str(node))
+
+    p = pathlib.Path("docs/test.py")
+    p.write_text(text)
+    # file = tempfile.NamedTemporaryFile("w")
+    # file.write(text)
+    serve_script(p)
+    # webbrowser.open("http://127.0.0.1:8000")
+
+
+# def serve_script(script_file: str | os.PathLike):
+#     script_file = pathlib.Path(script_file).absolute()
+#     script_file = script_file.relative_to(pathlib.Path.cwd())
+#     config = mkdocsconfig.Config()
+#     config.scripts = [script_file.as_posix()]
+#     output = yaml.dump(config._config, Dumper=yaml.Dumper)
+#     print(output)
+#     stream = io.StringIO(output)
+#     serve.serve(
+#         config_file=stream,  # type: ignore
+#         dev_addr=None,
+#         livereload=True,  # type: ignore
+#         build_type=None,
+#         watch_theme=False,
+#         watch=[],
+#     )
 
 
 def serve_script(script_file: str | os.PathLike):
     script_file = pathlib.Path(script_file).absolute()
     script_file = script_file.relative_to(pathlib.Path.cwd())
-    config = mkdocsconfig.Config()
-    config.scripts = [script_file.as_posix()]
-    output = yaml.dump(config._config, Dumper=yaml.Dumper)
+    text = pathlib.Path("mkdocs.yml").read_text()
+    config_file = yaml.load(text, Loader=yaml.Loader)
+    for plugin in config_file["plugins"]:
+        if isinstance(plugin, dict) and next(iter(plugin.keys())) == "mknodes":
+            plugin["mknodes"]["scripts"] = [script_file.as_posix()]
+    output = yaml.dump(config_file, Dumper=yaml.Dumper)
     stream = io.StringIO(output)
     serve.serve(
         config_file=stream,  # type: ignore
         dev_addr=None,
-        livereload="livereload",  # type: ignore
+        livereload=True,  # type: ignore
         build_type=None,
         watch_theme=False,
         watch=[],
@@ -28,6 +74,10 @@ def serve_script(script_file: str | os.PathLike):
 
 
 if __name__ == "__main__":
-    import sys
+    # import sys
 
-    serve_script(sys.argv[1])
+    # serve_script(sys.argv[1])
+    import mknodes
+
+    test = mknodes.MkCard("a", "b", "c")
+    serve_node(test)
