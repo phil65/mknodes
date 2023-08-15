2b38ef198202b5908d7bac3505c9fffd5e8838ce
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-15 03:41:05 +0200

    chore: cleanup

--- mknodes/utils/debugging.py
+++ mknodes/utils/debugging.py
@@ -7,16 +7,15 @@ import pathlib
 from mkdocs.commands import serve
 import yaml
 
+from mknodes import mkdocsconfig
+
 
 def serve_script(script_file: str | os.PathLike):
     script_file = pathlib.Path(script_file).absolute()
     script_file = script_file.relative_to(pathlib.Path.cwd())
-    text = pathlib.Path("mkdocs.yml").read_text()
-    config_file = yaml.load(text, Loader=yaml.Loader)
-    for plugin in config_file["plugins"]:
-        if isinstance(plugin, dict) and next(iter(plugin.keys())) == "gen-files":
-            plugin["gen-files"]["scripts"] = [script_file.as_posix()]
-    output = yaml.dump(config_file, Dumper=yaml.Dumper)
+    config = mkdocsconfig.Config()
+    config.scripts = [script_file.as_posix()]
+    output = yaml.dump(config._config, Dumper=yaml.Dumper)
     stream = io.StringIO(output)
     serve.serve(
         config_file=stream,  # type: ignore
