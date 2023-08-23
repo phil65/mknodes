70500cfcc491f0bea5262e8bb9256542bd41c255
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-08-23 19:26:45 +0200

    refactor: new style for MkCommandOutput

--- mknodes/templatenodes/mkcommandoutput.py
+++ mknodes/templatenodes/mkcommandoutput.py
@@ -3,37 +3,48 @@ from __future__ import annotations
 from collections.abc import Sequence
 import logging
 
-from mknodes.basenodes import mkcode
+from mknodes.basenodes import mknode
 from mknodes.utils import helpers
 
 
 logger = logging.getLogger(__name__)
 
 
-class MkCommandOutput(mkcode.MkCode):
+TEXT = """<div data-terminal>
+  <span data-ty="input">{input}</span>
+  <span data-ty>{output}</span>
+</div>"""
+
+
+class MkCommandOutput(mknode.MkNode):
     """Node to display the terminal output of a command."""
 
     ICON = "material/bash"
     STATUS = "new"
+    CSS = "css/terminal.css"
 
     def __init__(self, call: Sequence[str], **kwargs):
         self.call = call
         # caching on instance level as a compromise
         self._cache: dict[str, str] = {}  # {call: output}
-        super().__init__(language="", **kwargs)
+        super().__init__(**kwargs)
 
     def __repr__(self):
         return helpers.get_repr(self, call=self.call)
 
     @property
-    def text(self):
+    def output(self):
         key = " ".join(self.call)
         if key in self._cache:
             return self._cache[key]
         if output := helpers.get_output_from_call(self.call):
-            self._cache[key] = output
+            self._cache[key] = output.replace("\n", "<br>").replace(" ", "&nbsp;")
             return self._cache[key]
         return "**Command failed**"
+
+    def _to_markdown(self):
+        call = " ".join(self.call)
+        return TEXT.format(input=call, output=self.output)
 
     @staticmethod
     def create_example_page(page):
