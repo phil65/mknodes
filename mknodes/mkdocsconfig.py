9284ce49f8b0af77eedf2f4c1932811170055a98
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-01 04:18:53 +0200

    chore: add MkDocsConfig.get_page

--- mknodes/mkdocsconfig.py
+++ mknodes/mkdocsconfig.py
@@ -12,7 +12,7 @@ import markdown
 from mkdocs import config as _config
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import get_plugin_logger
-from mkdocs.structure.files import File
+from mkdocs.structure import files, pages
 from mkdocs.utils import write_file
 
 from mknodes.utils import helpers
@@ -148,22 +148,34 @@ class Config:
         path: str | os.PathLike,
         src_dir: str | os.PathLike | None = None,
         dest_dir: str | os.PathLike | None = None,
-    ) -> File:
+        inclusion_level: files.InclusionLevel = files.InclusionLevel.UNDEFINED,
+    ) -> files.File:
         """Return a MkDocs File for given path.
 
         Arguments:
             path: path to get a File object for (relative to src_dir)
             src_dir: Source directory. If None, docs_dir is used.
             dest_dir: Target directory. If None, site_dir is used.
+            inclusion_level: Inclusion level of new file
         """
-        new_f = File(
+        new_f = files.File(
             str(path),
             src_dir=str(src_dir) if src_dir else self._config.docs_dir,
             dest_dir=str(dest_dir) if dest_dir else self._config.site_dir,
             use_directory_urls=self._config.use_directory_urls,
+            inclusion=inclusion_level,
         )
         new_f.generated_by = "mknodes"  # type: ignore
         return new_f
+
+    def get_page(
+        self,
+        title: str,
+        path: str | os.PathLike,
+        inclusion_level: files.InclusionLevel = files.InclusionLevel.UNDEFINED,
+    ) -> pages.Page:
+        file = self.get_file(path, inclusion_level=inclusion_level)
+        return pages.Page(title, file, self._config)
 
     def get_code_repository(self) -> str:
         if self.repo_name:
