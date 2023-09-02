91981db99023021669b929151e054b2c73c2695f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-02 19:42:11 +0200

    chore: split up link_for_klass and link_for_module

--- mknodes/utils/linkprovider.py
+++ mknodes/utils/linkprovider.py
@@ -61,6 +61,18 @@ class LinkProvider:
     def add_inv_file(self, path: str | os.PathLike, base_url: str | None = None):
         self.inv_manager.add_inv_file(path, base_url=base_url)
 
+    def url_for_module(self, mod: types.ModuleType) -> str | None:
+        """Return a url for given module.
+
+        Arguments:
+            mod: Module to get a url for
+        """
+        dotted_path = mod.__name__
+        if dotted_path in self.inv_manager:
+            return self.inv_manager[dotted_path]
+        module = dotted_path.split(".")[0]
+        return homepage_for_distro(module)
+
     def link_for_module(self, mod: types.ModuleType) -> str:
         """Return a markdown link for given module.
 
@@ -68,13 +80,24 @@ class LinkProvider:
             mod: Module to get a link for
         """
         dotted_path = mod.__name__
+        if url := self.url_for_module(mod):
+            return linked(url, dotted_path)
+        return linked(dotted_path)
+
+    def url_for_klass(self, kls: type) -> str | None:
+        """Return a url for given class.
+
+        Arguments:
+            kls: Klass to get a url for
+        """
+        module_path = kls.__module__
+        qual_name = kls.__qualname__.split("[")[0]  # to split off generics part
+        is_builtin = module_path == "builtins"
+        dotted_path = qual_name if is_builtin else f"{module_path}.{qual_name}"
         if dotted_path in self.inv_manager:
-            link = self.inv_manager[dotted_path]
-            return linked(link, title=dotted_path)
-        module = dotted_path.split(".")[0]
-        if url := homepage_for_distro(module):
-            return linked(url, title=mod.__name__)
-        return linked(mod.__name__)
+            return self.inv_manager[dotted_path]
+        module = module_path.split(".")[0]
+        return homepage_for_distro(module)
 
     def link_for_klass(self, kls: type) -> str:
         """Return a markdown link for given class.
@@ -82,16 +105,9 @@ class LinkProvider:
         Arguments:
             kls: Klass to get a link for
         """
-        module_path = kls.__module__
         qual_name = kls.__qualname__.split("[")[0]  # to split off generics part
-        is_builtin = module_path == "builtins"
-        dotted_path = qual_name if is_builtin else f"{module_path}.{qual_name}"
-        if dotted_path in self.inv_manager:
-            link = self.inv_manager[dotted_path]
-            return linked(link, title=qual_name)
-        module = module_path.split(".")[0]
-        if url := homepage_for_distro(module):
-            return linked(url, title=qual_name)
+        if url := self.url_for_klass(kls):
+            return linked(url, qual_name)
         return linked(qual_name)
 
     def get_link(self, target) -> str:  # type: ignore[return]
