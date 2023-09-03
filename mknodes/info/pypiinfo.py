ba06acb368381d66870d8bbd1b586f9acdf6e7dd
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-03 19:37:49 +0200

    chore: prep

---
+++ mknodes/info/pypiinfo.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import logging
+
+from mknodes.utils import cache, helpers
+
+
+logger = logging.getLogger(__name__)
+
+URL = "https://pypi.org/pypi/{name}/json"
+
+
+class PyPiInfo:
+    def __init__(self, pkg_name: str):
+        self.package_name = pkg_name
+        url = URL.format(name=self.package_name)
+        self.response = cache.download_and_cache_url(url, days=1).decode()
+
+
+if __name__ == "__main__":
+    info = PyPiInfo("git_changelog")
+    print(info.response)
