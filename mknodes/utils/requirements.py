07da51a21a23628b0fb328370b0d789c5ac79474
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-04 18:22:58 +0200

    chore: add Requirements.merge

--- mknodes/utils/requirements.py
+++ mknodes/utils/requirements.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
+import abc
 import collections.abc
 import dataclasses
+
+import mergedeep
 
 from mknodes.pages import pagetemplate
 
 
 @dataclasses.dataclass
-class Requirements(collections.abc.Mapping):
+class Requirements(collections.abc.Mapping, metaclass=abc.ABCMeta):
     css: dict[str, str] = dataclasses.field(default_factory=dict)
     templates: list[pagetemplate.PageTemplate] = dataclasses.field(default_factory=list)
     markdown_extensions: dict[str, dict] = dataclasses.field(default_factory=dict)
@@ -26,7 +29,14 @@ class Requirements(collections.abc.Mapping):
     def __iter__(self):
         return iter(i.name for i in dataclasses.fields(self))
 
+    def merge(self, other: collections.abc.Mapping, additive: bool = False):
+        strategy = mergedeep.Strategy.ADDITIVE if additive else mergedeep.Strategy.REPLACE
+        result = dict(mergedeep.merge(self, other, strategy=strategy))
+        for k, v in result.items():
+            self[k] = v
+
 
 if __name__ == "__main__":
-    req = Requirements()
-    print(dict(req))
+    req = Requirements(css={"a.css": "CSS"})
+    req.merge(dict(css={"b.css": "CSS"}))
+    print(req)
