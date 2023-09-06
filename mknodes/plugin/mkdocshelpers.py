d88644f2ac73abb4a4e05fd2714e4839b9acf024
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-06 23:03:01 +0200

    chore: mkdocs helpers module

---
+++ mknodes/plugin/mkdocshelpers.py
@@ -0,0 +1,24 @@
+"""The Mkdocs Plugin."""
+
+from __future__ import annotations
+
+import pathlib
+
+from mkdocs.plugins import get_plugin_logger
+from mkdocs.structure.files import File, Files
+
+
+logger = get_plugin_logger(__name__)
+
+
+def merge_files(*files: Files) -> Files:
+    file_list = [i for j in files for i in j]
+
+    def file_sort_key(f: File):
+        parts = pathlib.PurePath(f.src_path).parts
+        return tuple(
+            chr(f.name != "index" if i == len(parts) - 1 else 2) + p
+            for i, p in enumerate(parts)
+        )
+
+    return Files(sorted(file_list, key=file_sort_key))
