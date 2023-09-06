8446f57138ba04ebf81fb8d14418570d9c6530a9
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-06 05:45:00 +0200

    chore: silence ruff

--- mknodes/info/packageinfo.py
+++ mknodes/info/packageinfo.py
@@ -107,8 +107,10 @@ registry: dict[str, PackageInfo] = {}
 def get_package_map() -> Mapping[str, list[str]]:
     return metadata.packages_distributions()
 
+
 def distribution_to_package(dist):
     return next((k for k, v in get_package_map().items() if dist in v), dist)
+
 
 def get_info(mod_name: str) -> PackageInfo:
     mapping = get_package_map()
