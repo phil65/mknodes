25651a2f99c2205dba7635b4d29ed9c7ad82e609
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-06 09:11:46 +0200

    chore: prep

--- mknodes/cli.py
+++ mknodes/cli.py
@@ -100,6 +100,54 @@ def serve(repo_url, site_script: str, clone_depth: int = 1, config_file=None, **
     serve_.serve(config_file=stream, livereload=False, **kwargs)  # type: ignore[arg-type]
 
 
+# @cli.command()
+# @click.option("-r", "--repo-url", help="Repo url of package to create a config for.")
+# @click.option(
+#     "-s",
+#     "--site-script",
+#     help="Path where config should get created.",
+#     default="mknodes.yml",
+# )
+# @click.option(
+#     "-p",
+#     "--config-path",
+#     help="Path where config file should get written to.",
+#     default="mknodes.mkwebsite:MkWebSite.for_project",
+# )
+# @click.option(
+#     "-t",
+#     "--theme",
+#     type=click.Choice(mkdocs.theme_choices),
+#     help="Theme to use",
+# )
+# @click.option(
+#     "--use-directory-urls/--no-directory-urls",
+#     is_flag=True,
+#     default=None,
+#     help=mkdocs.use_directory_urls_help,
+# )
+# @mkdocs.common_options
+# def create_config(config_path, repo_url, site_script: str, **kwargs):
+#     """Serve a MkNodes-based website."""
+#     mknodes_plugin = dict(
+#         mknodes={"repo_path": repo_url, "path": site_script, "clone_depth": 1},
+#     )
+#     cfg = {
+#         "site_name": "Not set",
+#         "plugins": [
+#             "search",
+#             "section-index",
+#             "mkdocstrings",
+#             "literate-nav",
+#             mknodes_plugin,
+#         ],
+#     }
+#     text = yamlhelpers.dump_yaml(cfg)
+#     buffer = io.StringIO(text)
+#     config = load_config(buffer, **kwargs)
+#     build_.build(config)
+
+
 def serve_node(node, repo_path: str = "."):
     text = f"""
     import mknodes
