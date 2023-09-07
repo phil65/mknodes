92dfadb6598dbf076a946f2ed15563e2e54f567e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-07 02:24:11 +0200

    chore: cleanup

--- mknodes/cli.py
+++ mknodes/cli.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
-import io
 import logging
-import pathlib
 import sys
 
 import click
 
 from mkdocs import __main__ as mkdocs
-from mkdocs.commands import build as build_, serve as serve_
-from mkdocs.config import load_config
 
-from mknodes.utils import yamlhelpers
+from mknodes.plugin import mkdocshelpers
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -49,19 +45,14 @@ def build(
     **kwargs,
 ):
     """Create a MkNodes-based website."""
-    cfg = yamlhelpers.load_yaml_file("mkdocs_basic.yml")
-    for plugin in cfg["plugins"]:
-        if "mknodes" in plugin:
-            plugin["mknodes"]["repo_path"] = repo_url
-            plugin["mknodes"]["path"] = site_script
-            plugin["mknodes"]["clone_depth"] = clone_depth
+    cfg = mkdocshelpers.load_and_patch_config(
+        config_file,
+        repo_url=repo_url,
+        site_script=site_script,
+        clone_depth=clone_depth,
+    )
     cfg["site_dir"] = site_dir
-    text = yamlhelpers.dump_yaml(cfg)
-    buffer = io.StringIO(text)
-    config = load_config(buffer, **kwargs)
-    config["plugins"].run_event("startup", command="build", dirty=False)
-    build_.build(config)
-    config["plugins"].run_event("shutdown")
+    mkdocshelpers.build(cfg, **kwargs)
 
 
 @cli.command()
@@ -83,15 +74,13 @@ def build(
 @mkdocs.common_options
 def serve(repo_url, site_script: str, clone_depth: int = 1, config_file=None, **kwargs):
     """Serve a MkNodes-based website."""
-    cfg = yamlhelpers.load_yaml_file(config_file or "mkdocs_basic.yml")
-    for plugin in cfg["plugins"]:
-        if "mknodes" in plugin:
-            plugin["mknodes"]["repo_path"] = repo_url
-            plugin["mknodes"]["path"] = site_script
-            plugin["mknodes"]["clone_depth"] = clone_depth
-    text = yamlhelpers.dump_yaml(cfg)
-    stream = io.StringIO(text)
-    serve_.serve(config_file=stream, livereload=False, **kwargs)  # type: ignore[arg-type]
+    cfg = mkdocshelpers.load_and_patch_config(
+        config_file,
+        repo_url=repo_url,
+        site_script=site_script,
+        clone_depth=clone_depth,
+    )
+    mkdocshelpers.serve(cfg, **kwargs)
 
 
 # @cli.command()
@@ -142,32 +131,5 @@ def serve(repo_url, site_script: str, clone_depth: int = 1, config_file=None, **
 #     build_.build(config)
 
 
-def serve_node(node, repo_path: str = "."):
-    text = f"""
-    import mknodes
-
-    def build(project):
-        root = project.get_root()
-        page = root.add_index_page(hide_toc=True)
-        page += '''{node!s}'''
-
-
-    """
-    p = pathlib.Path("docs/test.py")
-    p.write_text(text)
-    serve(repo_url=repo_path, site_script=p)
-
-
 if __name__ == "__main__":
     cli(sys.argv)
-    # cli(
-    #     [
-    #         "create",
-    #         "-r",
-    #         "https://github.com/mkdocs/mkdocs.git",
-    #         "-s",
-    #         "mknodes.mkwebsite:MkWebSite.for_project",
-    #         "-d",
-    #         "site/test",
-    #     ],
-    # )
--- mknodes/plugin/mkdocshelpers.py
+++ mknodes/plugin/mkdocshelpers.py
@@ -2,10 +2,20 @@
 
 from __future__ import annotations
 
+from collections.abc import Mapping
+import io
+import os
 import pathlib
 
+from typing import Any
+
+from mkdocs.commands import build as build_, serve as serve_
+from mkdocs.config import load_config
+from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import get_plugin_logger
 from mkdocs.structure.files import File, Files
+
+from mknodes.utils import yamlhelpers
 
 
 logger = get_plugin_logger(__name__)
@@ -22,3 +32,62 @@ def merge_files(*files: Files) -> Files:
         )
 
     return Files(sorted(file_list, key=file_sort_key))
+
+
+def build(config: MkDocsConfig | Mapping[str, Any], **kwargs):
+    match config:
+        case Mapping():
+            text = yamlhelpers.dump_yaml(config)
+            buffer = io.StringIO(text)
+            config = load_config(buffer, **kwargs)
+    config.plugins.run_event("startup", command="build", dirty=False)
+    build_.build(config)
+    config.plugins.run_event("shutdown")
+
+
+def serve(
+    config: str | os.PathLike | MkDocsConfig | Mapping[str, Any] = "mkdocs_basic.yml",
+    **kwargs,
+):
+    """Serve a MkNodes-based website."""
+    match config:
+        case str() | os.PathLike():
+            cfg = yamlhelpers.load_yaml_file(config)
+        case dict():
+            cfg = config
+        case MkDocsConfig():
+            cfg = dict(config)
+    text = yamlhelpers.dump_yaml(cfg)
+    stream = io.StringIO(text)
+    serve_.serve(config_file=stream, livereload=False, **kwargs)  # type: ignore[arg-type]
+
+
+def serve_node(node, repo_path: str = "."):
+    text = f"""
+    import mknodes
+
+    def build(project):
+        root = project.get_root()
+        page = root.add_index_page(hide_toc=True)
+        page += '''{node!s}'''
+
+
+    """
+    p = pathlib.Path("docs/test.py")
+    p.write_text(text)
+    serve(repo_url=repo_path, site_script=p)
+
+
+def load_and_patch_config(
+    path,
+    repo_url: str = ".",
+    site_script: str = "",
+    clone_depth: int = 1,
+):
+    cfg = yamlhelpers.load_yaml_file(path or "mkdocs_basic.yml")
+    for plugin in cfg["plugins"]:
+        if "mknodes" in plugin:
+            plugin["mknodes"]["repo_path"] = repo_url
+            plugin["mknodes"]["path"] = site_script
+            plugin["mknodes"]["clone_depth"] = clone_depth
+    return cfg
