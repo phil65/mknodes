ee9b358ee3bfaa916c9e835d997cf8ed5d59ba33
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-08 05:48:00 +0200

    feat: add cli `create-config` command

--- mknodes/cli/__init__.py
+++ mknodes/cli/__init__.py
@@ -8,9 +8,12 @@ import click
 from mknodes.cli import cli_options
 
 from mknodes.plugin import mkdocshelpers
+from mknodes.utils import yamlhelpers
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -61,32 +64,64 @@ def serve(repo_url, site_script: str, clone_depth: int = 1, config_file=None, **
     mkdocshelpers.serve(cfg, **kwargs)
 
 
-# @cli.command()
-# @cli_options.repo_url_option
-# @cli_options.site_script_option
-# @cli_options.config_path_option
-# @cli_options.theme_option
-# @cli_options.directory_urls_option
-# @cli_options.debug_options # verbose / quiet
-# def create_config(config_path, repo_url, site_script: str, **kwargs):
-#     """Serve a MkNodes-based website."""
-#     mknodes_plugin = dict(
-#         mknodes={"repo_path": repo_url, "path": site_script, "clone_depth": 1},
-#     )
-#     cfg = {
-#         "site_name": "Not set",
-#         "plugins": [
-#             "search",
-#             "section-index",
-#             "mkdocstrings",
-#             "literate-nav",
-#             mknodes_plugin,
-#         ],
-#     }
-#     text = yamlhelpers.dump_yaml(cfg)
-#     buffer = io.StringIO(text)
-#     config = load_config(buffer, **kwargs)
-#     build_.build(config)
+@cli.command()
+@cli_options.repo_url_option
+@cli_options.site_script_option
+@cli_options.config_path_option
+@cli_options.theme_option
+@cli_options.use_directory_urls_option
+@cli_options.debug_options  # verbose / quiet
+def create_config(repo_url, site_script: str, theme: str | None, **kwargs):
+    """Serve a MkNodes-based website."""
+    mknodes_plugin = dict(
+        mknodes={"repo_path": repo_url, "path": site_script},
+    )
+    theme_name = theme or "material"
+    config = {
+        "site_name": "Not set",
+        "theme": theme_name,
+        "extra": {},
+        "plugins": [
+            "search",
+            "section-index",
+            "mkdocstrings",
+            "literate-nav",
+            mknodes_plugin,
+        ],
+    }
+
+    from mknodes import project
+    from mknodes.theme import theme as theme_
+    from mknodes.info import folderinfo
+    from mknodes.utils import classhelpers, helpers
+
+    if helpers.is_url(repo_url):
+        repo = folderinfo.FolderInfo.clone_from(repo_url, depth=1)
+    else:
+        repo = repo_url
+
+    skin = theme_.Theme(theme_name)
+    proj = project.Project(
+        base_url="",
+        use_directory_urls=True,
+        theme=skin,
+        repo=repo,
+    )
+    project_fn = classhelpers.get_callable_from_path(site_script)
+    logger.debug("Building page...")
+    project_fn(project=proj)
+    logger.debug("Finished building page.")
+    proj.aggregate_info()
+    info = proj.infocollector
+    config["markdown_extensions"] = info["markdown_extensions"]
+    if social := info["metadata"]["social_info"]:
+        config["extra"]["social"] = social  # type: ignore[index]
+    config["repo_url"] = info["metadata"]["repository_url"]
+    config["site_description"] = info["metadata"]["summary"]
+    config["site_name"] = info["metadata"]["name"]
+    config["site_author"] = info["project"].info.author_name
+    result = yamlhelpers.dump_yaml(config)
+    print(result)
 
 
 if __name__ == "__main__":
