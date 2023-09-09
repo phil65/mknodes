7c0ef81406b97605ead15ca47314f32acf96c8d5
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-09 05:32:05 +0200

    chore: prep

--- mknodes/utils/log.py
+++ mknodes/utils/log.py
@@ -4,6 +4,18 @@ import io
 import logging
 
 
+class Logger:
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+
+    def __getattr__(self, val):
+        return getattr(self.logger, val)
+
+    def log_dict(self, dct, level=logging.INFO):
+        for k, v in dct.items():
+            self.logger.log(level, "%s: %s", k, v)
+
+
 def get_logger(name: str | None = None) -> logging.Logger:
     return logging.getLogger(f"mkdocs.plugin.{name}" if name else None)
 
