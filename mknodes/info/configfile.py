e9edebc199a8c42a6cfb1052b6ccf5f3622e2537
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-10 19:31:12 +0200

    chore: some work for ConfigFile

--- mknodes/info/configfile.py
+++ mknodes/info/configfile.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from abc import ABCMeta
+from collections.abc import MutableMapping
 import json
 import os
 import pathlib
@@ -9,25 +11,44 @@ from typing import Any
 from mknodes.utils import cache, helpers
 
 
-class ConfigFile:
-    def __init__(self, path: str | os.PathLike):
+class ConfigFile(MutableMapping, metaclass=ABCMeta):
+    def __init__(self, path: str | os.PathLike | None = None):
+        self._data: dict[str, Any] = {}
+        self.path = path
+        if self.path is None:
+            return
         self.path = str(path)
-        path = pathlib.Path(path)
         if helpers.is_url(self.path):
             content = cache.download_and_cache_url(self.path, days=1)
             self.load_config(content.decode())
         else:
-            self.load_file(path)
+            self.load_file(self.path)
 
     def __getitem__(self, value):
         if isinstance(value, str):
             return self._data.__getitem__(value)
         return self.get_section(value)
 
+    def __setitem__(self, index, value):
+        self._data[index] = value
+
+    def __delitem__(self, index):
+        del self._data[index]
+
     def __repr__(self):
         return f"{type(self).__name__}({self.path!r})"
 
+    def __bool__(self):
+        return bool(self._data or self.path)
+
+    def __iter__(self):
+        return iter(self._data.keys())
+
+    def __len__(self):
+        return len(self._data)
+
     def get_section(self, *sections: str, keep_path: bool = False) -> Any:
+        # sourcery skip: merge-duplicate-blocks
         """Try to get data[sections[0]][sections[1]]...
 
         If Key path does not exist, return None.
@@ -38,10 +59,21 @@ class ConfigFile:
         """
         section = self._data
         for i in sections:
-            if child := section.get(i):
-                section = child
+            if isinstance(section, dict):
+                if child := section.get(i):
+                    section = child
+                else:
+                    return None
             else:
-                return None
+                for idx in section:
+                    if i in idx and isinstance(idx, dict):
+                        section = idx[i]
+                        break
+                    if isinstance(idx, str) and idx == i:
+                        section = idx
+                        break
+                else:
+                    return None
         if not keep_path:
             return section
         result: dict[str, dict] = {}
@@ -72,7 +104,7 @@ class ConfigFile:
         return "" if section is None else json.dumps(section)
 
     def load_config(self, data: str):
-        self._data: dict = self._load(data)
+        self._data = self._load(data)
 
     def dump_config(self) -> str:
         return self._dump(self._data)
