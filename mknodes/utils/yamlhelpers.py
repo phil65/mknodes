0dd6597713e1f304895df378bf978cbf92b281d4
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-12 04:23:48 +0200

    chore: prep

--- mknodes/utils/yamlhelpers.py
+++ mknodes/utils/yamlhelpers.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import collections
+import contextlib
 import pathlib
 
 from typing import Any
@@ -11,6 +13,26 @@ from mknodes.utils import log, mergehelpers
 
 logger = log.get_logger(__name__)
 YAMLError = yaml.YAMLError
+
+
+def patch_dumper_to_not_order(dumper_cls):
+    def map_representer(dumper_cls_, data):
+        return dumper_cls_.represent_dict(data.items())
+
+    yaml.add_representer(dict, map_representer, Dumper=dumper_cls)
+    yaml.add_representer(collections.OrderedDict, map_representer, Dumper=dumper_cls)
+
+
+def patch_pyyaml_to_not_order_dicts():
+    _dumpers = [getattr(yaml.dumper, x) for x in yaml.dumper.__all__]
+    with contextlib.suppress(AttributeError):
+        _cyaml = yaml.cyaml.__all__
+        _dumpers += [getattr(yaml.cyaml, x) for x in _cyaml if x.endswith("Dumper")]
+    for dumper in _dumpers:
+        patch_dumper_to_not_order(dumper)
+
+
+# patch_pyyaml_to_not_order_dicts()
 
 
 def load_yaml_file(source, mode="unsafe", resolve_inherit: bool = True):
