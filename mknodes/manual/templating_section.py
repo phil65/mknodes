e39a577ff27335cc93843cd382412c9dc8ae628e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-15 06:41:15 +0200

    chore: use decorators for templating section

--- mknodes/manual/templating_section.py
+++ mknodes/manual/templating_section.py
@@ -14,24 +14,31 @@ SECTION_CODE = "Code for this section"
 PAGE_CODE = "Code for this page"
 
 
+templating_nav = mknodes.MkNav("Templating")
+
+
 def create_templating_section(root_nav: mknodes.MkNav):
     """Add the complete "Templating" section to given MkNav."""
-    templating_nav = root_nav.add_nav("Templating")
-    # first we create the menu on the left:
-
-    create_macros_nav(templating_nav)
-
-    # and then we create the index page (the page you are lookin at right now)
-
+    root_nav += templating_nav
     page = templating_nav.add_index_page(hide_toc=True, icon="simple/jinja")
     page += mknodes.MkCode.for_object(create_templating_section, header=SECTION_CODE)
     page += mknodes.MkDetailsBlock(INTRO_TEXT, expand=True)
 
 
-def create_macros_nav(nav: mknodes.MkNav):
-    macros_nav = nav.add_nav("Jinja macros")
+@templating_nav.route("Jinja macros", show_source=True)
+def create_macros_nav():
+    def add_context_doc(container, context):
+        container += mknodes.MkDocStrings(
+            context,
+            show_root_toc_entry=False,
+            show_if_no_docstring=True,
+            heading_level=4,
+            show_bases=False,
+            show_source=False,
+        )
+
+    macros_nav = mknodes.MkNav("Jinja macros")
     page = macros_nav.add_index_page(icon=mknodes.MkClassPage.ICON, hide_toc=True)
-    page += mknodes.MkCode.for_object(create_macros_nav, header=PAGE_CODE)
     add_context_doc(page, contexts.ProjectContext)
     for ctx in [
         contexts.PackageContext,
@@ -41,21 +48,4 @@ def create_macros_nav(nav: mknodes.MkNav):
     ]:
         page = macros_nav.add_page(ctx.__name__)
         add_context_doc(page, ctx)
-
-
-def add_context_doc(container, context):
-    container += mknodes.MkDocStrings(
-        context,
-        show_root_toc_entry=False,
-        show_if_no_docstring=True,
-        heading_level=4,
-        show_bases=False,
-        show_source=False,
-    )
-
-
-def create_filters_page(nav: mknodes.MkNav):
-    page = nav.add_page("Jinja macros", icon=mknodes.MkClassPage.ICON)
-    page += mknodes.MkCode.for_object(create_filters_page, header=PAGE_CODE)
-    class_page = mknodes.MkClassPage(mknodes.MkCode, inclusion_level=False)
-    page += mknodes.MkReprRawRendered(class_page)
+    return macros_nav
