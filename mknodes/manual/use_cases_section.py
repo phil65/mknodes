ee0426c1ff79c177893805fb7e0a9a316b6e27a9
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2023-09-23 00:12:56 +0200

    chore: cleanup

--- mknodes/manual/use_cases_section.py
+++ mknodes/manual/use_cases_section.py
@@ -3,37 +3,6 @@ import mknodes as mk
 from mknodes.info import mkdocsconfigfile
 
 
-INTRO_TEXT = """
-There are a lot of different ways MkNodes can be used to generate
-websites. This section will highlight some of them.
-
-
-**MkNodes** can be used in many different ways
-
-* Create complete websites
-    * **MkNodes** can be used to create a whole website (like it is done with this page)
-
-* Load existing websites and extend it
-    * By using `MkNav.parse.folder` / `MkNav.parse.file` as well as `MkPage.from_file`,
-      the existing Markdown pages can become part of our tree
-      (which we can then extend programatically)
-
-* Create a subsection with **MkNodes** and reference it from your "static" page.
-    * You can also set a section name for your root `MkNav` and reference that from your
-      `nav:` section in mkdocs.yml
-
-* Create single static pages / blocks for your page
-    You can also just use **MkNodes** to create some Nodes, stringify them and include
-    the markdown in your static pages
-
-* Using `Markdown-Exec` or similar inline execution plugins
-    You can also embed **MkNodes** code directly by using various plug-ins
-    (personal recommendation: `Markdown-Exec`)
-
-
-"""
-
-
 nav = mk.MkNav("Use cases")
 
 
@@ -41,7 +10,7 @@ def create_use_cases_section(root_nav: mk.MkNav):
     """Create the "Development" sub-MkNav."""
     root_nav += nav
     page = nav.add_index_page(hide="toc")
-    page += INTRO_TEXT
+    page += mk.MkJinjaTemplate("use_cases_index.jinja")
     page.created_by = create_use_cases_section
 
 
