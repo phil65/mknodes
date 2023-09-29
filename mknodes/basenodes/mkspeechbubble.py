a2f366768abce440e370d0ad1ab7b305b482313f
phil65 <philipptemminghoff@googlemail.com>
2023-09-30 01:06:51 +0200

    chore: xml-ize MkSpeechBubble

--- mknodes/basenodes/mkspeechbubble.py
+++ mknodes/basenodes/mkspeechbubble.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Literal
 
 from mknodes.basenodes import mkcontainer, mknode
-from mknodes.utils import log, reprhelpers, requirements
+from mknodes.utils import log, reprhelpers, requirements, xmlhelpers as xml
 
 
 logger = log.get_logger(__name__)
@@ -27,21 +27,23 @@ class MkSpeechBubble(mkcontainer.MkContainer):
     def __repr__(self):
         return reprhelpers.get_repr(self, content=self.items, arrow=self.arrow)
 
+    def get_element(self) -> xml.Div:
+        klass = f"speech {self.arrow}" if self.arrow else "speech"
+        root = xml.Div(klass, markdown=True)
+        root.text = super()._to_markdown()
+        return root
+
     def _to_markdown(self) -> str:
-        arrow_str = f" {self.arrow}" if self.arrow else ""
-        begin = f'<div class="speech{arrow_str}" markdown="1">\n'
-        content = super()._to_markdown()
-        end = "\n</div>"
-        return f"{begin}{content}{end}"
+        return self.get_element().to_string()
 
     @staticmethod
     def create_example_page(page):
-        import mknodes
+        import mknodes as mk
 
         node = MkSpeechBubble(MkSpeechBubble.__doc__)
-        page += mknodes.MkReprRawRendered(node)
+        page += mk.MkReprRawRendered(node)
         node = MkSpeechBubble(MkSpeechBubble.__doc__, arrow="left")
-        page += mknodes.MkReprRawRendered(node)
+        page += mk.MkReprRawRendered(node)
 
 
 if __name__ == "__main__":
