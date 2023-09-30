80870f960c21b9ccdb033cb4e6bbaac54c410897
phil65 <philipptemminghoff@googlemail.com>
2023-09-30 09:15:19 +0200

    chore: make kwargs optional for mdconverter

--- mknodes/utils/mdconverter.py
+++ mknodes/utils/mdconverter.py
@@ -14,8 +14,8 @@ logger = log.get_logger(__name__)
 class MdConverter(markdown.Markdown):
     def __init__(
         self,
-        extensions: Sequence[str | markdown.Extension],
-        extension_configs: dict[str, dict[str, Any]],
+        extensions: Sequence[str | markdown.Extension] | None = None,
+        extension_configs: dict[str, dict[str, Any]] | None = None,
         output_format: Literal["xhtml", "html"] = "html",
         tab_length: int = 4,
     ):
@@ -24,7 +24,7 @@ class MdConverter(markdown.Markdown):
             exts = list({*exts, *extensions})
         super().__init__(
             extensions=exts,
-            extension_configs=extension_configs,
+            extension_configs=extension_configs or {},
             output_format=output_format,
             tab_length=tab_length,
         )
