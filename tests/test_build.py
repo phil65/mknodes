baddc7d09ffbb6fcfbb2c227ad142fd28eb59697
phil65 <philipptemminghoff@googlemail.com>
2023-10-15 19:34:37 +0200

    test: add build tests

---
+++ tests/test_build.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import mknodes as mk
+
+
+def build(project):
+    root = project.get_root()
+    sub_nav = mk.MkNav("Sub nav")
+    sub_nav.page_template.announcement_bar = "Hello"
+    sub_nav += mk.MkPage("Test page")
+    root += sub_nav
+
+
+def test_templates(project):
+    theme = mk.MaterialTheme()
+    project = mk.Project(theme=theme, repo=".", build_fn=build)
+    project.build()
+    assert project._root
+    resources = project._root.get_resources()
+    assert len(resources.templates) == 1
