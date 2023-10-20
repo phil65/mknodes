b877c8219c3d53cf0ddc2583f82e8dfc94525d8c
phil65 <philipptemminghoff@googlemail.com>
2023-10-20 08:20:30 +0200

    chore: cleanup

--- mknodes/paths.py
+++ mknodes/paths.py
@@ -5,8 +5,9 @@ from __future__ import annotations
 import pathlib
 
 
-TEST_RESOURCES = pathlib.Path(__file__).parent.parent / "tests" / "data"
-RESOURCES = pathlib.Path(__file__).parent / "resources"
-CSS_DIR = RESOURCES / "css"
-DOCS_DIR = pathlib.Path(__file__).parent.parent / "docs"
-DEFAULT_BUILD_FN = "mknodes.navs.mkdefaultwebsite:MkDefaultWebsite.for_project"
+ROOT = pathlib.Path(__file__).parent
+
+TEST_RESOURCES = ROOT.parent / "tests" / "data"
+RESOURCES = ROOT / "resources"
+
+DEFAULT_BUILD_FN = "mknodes:MkDefaultWebsite.for_project"
