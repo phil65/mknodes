712a51468716f2e7e79e335ed8cbc31e15d7ab3f
phil65 <philipptemminghoff@googlemail.com>
2023-10-23 17:55:09 +0200

    chore: test work

--- tests/test_nodefiles.py
+++ tests/test_nodefiles.py
@@ -10,24 +10,37 @@ from mknodes.utils import classhelpers
 
 
 def node_instances():
+    theme = mk.Theme("mkdocs")
+    proj = mk.Project(theme=theme)
     for cls in classhelpers.iter_subclasses(mk.MkNode):
         with contextlib.suppress(TypeError):
-            yield cls.with_default_context()
+            yield cls(context=proj.context)
 
 
-@pytest.mark.parametrize("all_nodes", node_instances())
-def test_examples(all_nodes):
-    for node in all_nodes:
-        try:
-            nodefile = node.get_nodefile()
-            for v in nodefile.examples.values():
-                result = node.env.render_string(v["jinja"])
-                assert result
-            # node.env.render_string()
-        except (FileNotFoundError, TypeError):
-            pass
+@pytest.mark.parametrize("node", node_instances())
+def test_examples(node):
+    try:
+        nodefile = node.get_nodefile()
+    except FileNotFoundError:
+        pass
+    else:
+        for v in nodefile.examples.values():
+            node.env.render_string(v["jinja"])
+
+
+@pytest.mark.parametrize("node", node_instances())
+def test_output(node):
+    try:
+        nodefile = node.get_nodefile()
+    except FileNotFoundError:
+        pass
+    else:
+        if (output := nodefile.get("output")) and (xml := output.get("xml")):
+            raise ValueError(xml)
+            result = node.env.render_string(xml)
+            assert result
 
 
 # theme = mk.MaterialTheme()
 # proj = mk.Project(theme=theme)
-# test_examples(proj)
+# test_output(proj)
