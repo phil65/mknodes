d071eb764af7bbb71d8238c4460c31745d727c8e
phil65 <philipptemminghoff@googlemail.com>
2023-10-24 18:01:33 +0200

    chore: implement detail=True for jinjaloaderfilesystem

--- mknodes/jinja/jinjaloaderfilesystem.py
+++ mknodes/jinja/jinjaloaderfilesystem.py
@@ -25,17 +25,42 @@ class JinjaLoaderFileSystem(fsspec.AbstractFileSystem):
         super().__init__()
         self.env = env
 
-    def ls(self, path: str, detail: bool = True, **kwargs) -> list[str]:
+    def ls(self, path: str, detail: bool = True, **kwargs) -> list:
+        """Implementation for AbstractFileSystem."""
         if not self.env.loader:
             return []
         paths = self.env.loader.list_templates()
-        if not path:
-            return paths
-        path = pathlib.Path(path).as_posix().rstrip("/")
-        folders = [i for i in paths if i.rsplit("/", 1)[0] == path]
-        if not folders:
+        path = pathlib.Path(path).as_posix().strip("/")
+        if path in {"", "/", "."}:
+            """Root, return all."""
+            if detail:
+                files = [{"name": p, "type": "file"} for p in paths if "/" not in p]
+                dirs = [
+                    {"name": p.split("/")[0], "type": "directory"}
+                    for p in paths
+                    if p.count("/") >= 1 and p not in files
+                ]
+                dirs = [i for n, i in enumerate(dirs) if i not in dirs[n + 1 :]]
+                return dirs + files
+            files = [p for p in paths if "/" not in p]
+            dirs = [
+                p.split("/")[0] for p in paths if p.count("/") >= 1 and p not in files
+            ]
+            return list(set(dirs)) + files
+        if detail:
+            items = [
+                {
+                    "name": i,
+                    "type": "file" if "." in pathlib.Path(i).name else "directory",
+                }
+                for i in paths
+                if i.rsplit("/", 1)[0] == path
+            ]
+        else:
+            items = [i for i in paths if i.rsplit("/", 1)[0] == path]
+        if not items:
             raise FileNotFoundError(path)
-        return folders
+        return items
 
     def _open(self, path: str, mode="rb", **kwargs) -> io.BytesIO:
         if not self.env.loader:
@@ -51,6 +76,6 @@ if __name__ == "__main__":
     fsspec.register_implementation("jinja", JinjaLoaderFileSystem)
     env = jinja2.Environment(loader=loaders_.resource_loader)
     fs = fsspec.filesystem("jinja", env=env)
-    # print(fs.ls(""))
-    with fs.open("licenses/templates/TORQUE-1.1.txt") as file:
-        print(file.read())
+    print(fs.ls(""))
+    # with fs.open("licenses/templates/TORQUE-1.1.txt") as file:
+    #     print(file.read())
