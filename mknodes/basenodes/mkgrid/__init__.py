720a0db69cc32c05d937548e6929f4af46d1b6a3
phil65 <philipptemminghoff@googlemail.com>
2023-10-28 15:59:37 +0200

    chore: cleanup MkGrid

--- mknodes/basenodes/mkgrid/__init__.py
+++ mknodes/basenodes/mkgrid/__init__.py
@@ -1,68 +1,43 @@
 from __future__ import annotations
+from typing import Any
 
 from mknodes.basenodes import mkcontainer, mknode
-from mknodes.utils import log, reprhelpers, xmlhelpers as xml
+from mknodes.utils import log, xmlhelpers as xml
 
 
 logger = log.get_logger(__name__)
-
-
-class MkGridCard(mkcontainer.MkContainer):
-    """Node representing a single grid card."""
-
-    ICON = "material/square-medium"
-    STATUS = "new"
-
-    def __init__(
-        self,
-        content: list | str | mknode.MkNode | None = None,
-        **kwargs,
-    ):
-        super().__init__(content=content, **kwargs)
-
-    def _to_markdown(self) -> str:
-        item_str = "\n".join(i.to_markdown() for i in self.items)
-        lines = item_str.split("\n")
-        result = [f"-   {lines[0]}"]
-        result.extend(f"    {i}" for i in lines[1:])
-        return "\n".join(result) + "\n"
 
 
 class MkGrid(mkcontainer.MkContainer):
     """Node for showing a grid."""
 
     ICON = "material/view-grid"
-    items: list[MkGridCard]
 
-    def __init__(
-        self,
-        cards: list[str | mknode.MkNode] | None = None,
-        *,
-        header: str = "",
-        **kwargs,
-    ):
-        match cards:
+    def __init__(self, items: list[str | mknode.MkNode] | None = None, **kwargs: Any):
+        """Initialize the node.
+
+        Arguments:
+            items: The items of this node.
+            kwargs: Keyword arguments passed to parent
+        """
+        match items:
             case None:
                 items = []
             case list():
-                items = [
-                    (card if isinstance(card, MkGridCard) else MkGridCard(card))
-                    for card in cards
-                ]
-        super().__init__(content=items, header=header, **kwargs)
-
-    def __repr__(self):
-        cards = []
-        for item in self.items:
-            if len(item.items) == 1:
-                cards.append(reprhelpers.to_str_if_textnode(item.items[0]))
-            else:
-                cards.append(item)
-        return reprhelpers.get_repr(self, cards=cards)
+                items = [self.to_child_node(i) for i in items]
+        super().__init__(content=items, **kwargs)
 
     def get_element(self) -> xml.Div:
         root = xml.Div("grid cards", markdown=True)
-        content = "".join(str(i) for i in self.items) if self.items else ""
+        if not self.items:
+            return root
+        result = []
+        for item in self.items:
+            item_str = item.to_markdown()
+            lines = item_str.split("\n")
+            result += [f"-   {lines[0]}"]
+            result.extend(f"    {i}" for i in lines[1:])
+        content = "\n".join(result)
         root.text = "\n\n" + content + "\n"
         return root
 
@@ -78,7 +53,7 @@ class MkGrid(mkcontainer.MkContainer):
         ls = ["Item 1", "Item 2", "Item 3"]
         item_1 = mk.MkList(ls)
         item_2 = mk.MkKeys(keys=["Ctrl+A"])
-        grid = MkGrid(cards=[item_1, item_2])
+        grid = MkGrid([item_1, item_2])
         page += mk.MkReprRawRendered(grid)
 
 
@@ -86,5 +61,5 @@ if __name__ == "__main__":
     import mknodes as mk
 
     keys = mk.MkKeys(keys="Ctrl+A")
-    grid = MkGrid(cards=[keys, keys])
+    grid = MkGrid([keys, keys])
     print(grid)
