abbf566fa5cef5b1e3baa0f81490017074d377cd
phil65 <philipptemminghoff@googlemail.com>
2023-10-31 13:03:29 +0100

    test: add a test for MkText jinja children

--- tests/test_text.py
+++ tests/test_text.py
@@ -22,3 +22,14 @@ def test_fetch_from_url():
     url = "https://raw.githubusercontent.com/fire1ce/DDNS-Cloudflare-Bash/main/README.md"
     node = mk.MkText.from_url(url)
     assert str(node)
+
+
+def test_rendered_children():
+    node = mk.MkText("{{ 'Test' | MkAdmonition | MkAdmonition }}", render_jinja=True)
+    num_desc = 3  # 2 * MkAdmonition, 1 * MkText
+    assert len(node.children) == 1
+    assert len(list(node.descendants)) == num_desc
+
+    node = mk.MkText("{{ mk.MkAdmonition(mk.MkAdmonition('test')) }}", render_jinja=True)
+    assert len(node.children) == 1
+    assert len(list(node.descendants)) == num_desc
