6f1e276d900738f5cba25d73008fc2f2379f6791
phil65 <philipptemminghoff@googlemail.com>
2023-11-12 18:37:47 +0100

    refactor: make MkConfigSetting inherit from MkTemplate

--- mknodes/templatenodes/mkconfigsetting/__init__.py
+++ mknodes/templatenodes/mkconfigsetting/__init__.py
@@ -2,18 +2,14 @@ from __future__ import annotations
 
 from typing import Any
 
-from mknodes.basenodes import mkdefinitionlist
-from mknodes.utils import helpers, log, superdict
+from mknodes.templatenodes import mktemplate
+from mknodes.utils import superdict
 
 
-logger = log.get_logger(__name__)
-
-
-class MkConfigSetting(mkdefinitionlist.MkDefinition):
+class MkConfigSetting(mktemplate.MkTemplate):
     """Node for describing a config setting."""
 
     ICON = "material/library"
-    VIRTUAL_CHILDREN = True
 
     def __init__(
         self,
@@ -39,44 +35,14 @@ class MkConfigSetting(mkdefinitionlist.MkDefinition):
             mode: Markup language of settings file
             kwargs: Keyword arguments passed to parent
         """
-        super().__init__(content=None, title=title, **kwargs)
+        super().__init__("output/markdown/template", **kwargs)
+        self.title = title
         self.setting = setting
         self.default = default
         self.version_added = version_added
         self.description = description
         self.optional = optional
         self.mode = mode
-
-    @property
-    def title(self) -> str:
-        return helpers.styled(self._title, bold=True, code=True)
-
-    @property
-    def setting_string(self) -> str | None:
-        if isinstance(self.setting, dict):
-            return superdict.SuperDict(self.setting).serialize(mode=self.mode)
-        return self.setting
-
-    @property
-    def items(self):
-        import mknodes as mk
-
-        text = f"Default: `{self.default!r}`\n\n"
-        if self.version_added:
-            text += f"Version added: `{self.version_added}`\n\n"
-        if self.optional is not None:
-            required = "no" if self.optional else "yes"
-            text += f"Required: `{required}`\n\n"
-        text += f"{self.description}\n"
-        items: list[mk.MkNode] = [mk.MkText(text, parent=self)]
-        if self.setting_string:
-            code_node = mk.MkCode(self.setting_string, parent=self, language=self.mode)
-            items.append(code_node)
-        return items
-
-    @items.setter
-    def items(self, value):
-        pass
 
     @classmethod
     def create_example_page(cls, page):
