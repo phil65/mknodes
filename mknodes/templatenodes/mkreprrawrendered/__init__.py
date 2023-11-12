5624bb88d2923cbc2c5715821bf9476150bce9ff
phil65 <philipptemminghoff@googlemail.com>
2023-11-13 00:24:48 +0100

    refactor: make MkReprRawRendr inherit from MkTemplate

--- mknodes/templatenodes/mkreprrawrendered/__init__.py
+++ mknodes/templatenodes/mkreprrawrendered/__init__.py
@@ -2,14 +2,11 @@ from __future__ import annotations
 
 from typing import Any
 
-from mknodes.basenodes import mknode, mktabbed, mktabs
-from mknodes.utils import log, reprhelpers
+from mknodes.templatenodes import mktemplate
+from mknodes.basenodes import mknode
 
 
-logger = log.get_logger(__name__)
-
-
-class MkReprRawRendered(mktabbed.MkTabbed):
+class MkReprRawRendered(mktemplate.MkTemplate):
     """Node showing a tabbed block to visualize a node in different representations.
 
     It contains a tab for the repr, one for the rendered output,
@@ -19,65 +16,27 @@ class MkReprRawRendered(mktabbed.MkTabbed):
     """
 
     ICON = "material/presentation"
-    VIRTUAL_CHILDREN = True
 
-    def __init__(
-        self,
-        node: mknode.MkNode | str | None = None,
-        select_tab: int | str | None = 3,
-        **kwargs: Any,
-    ):
+    def __init__(self, node: mknode.MkNode | str | None = None, **kwargs: Any):
         """Constructor.
 
         Arguments:
             node: Node to show an example for
-            select_tab: Tab which should be selected initially
             kwargs: Keyword arguments passed to parent
         """
-        self.node = node
-        super().__init__(tabs={}, select_tab=select_tab, **kwargs)
-
-    def __repr__(self):
-        return reprhelpers.get_repr(self, node=self.node)
+        self._node = node
+        super().__init__("output/markdown/template", **kwargs)
 
     @property
-    def items(self):
-        import mknodes as mk
-
-        match self.node:
+    def node(self):
+        match self._node:
             case None:
-                return []
+                return None
             case str():
-                self.env.render_string(self.node)
-                node = self.env.rendered_children[0]
+                self.env.render_string(self._node)
+                return self.env.rendered_children[0]
             case _:
-                node = self.node
-        html_node = node.__copy__()
-        html_node.as_html = True
-        tabs: dict[str, str | mk.MkNode] = dict(  # type: ignore[annotation-unchecked]
-            Repr=mk.MkCode(repr(node)),
-            Raw=mk.MkCode(node, language="markdown"),
-            Html=mk.MkCode(html_node, language="html"),
-            Rendered=node.__copy__(),
-        )
-        if isinstance(self.node, str):
-            tabs = {"Jinja": self.node, **tabs}
-        if len(node.children) > 0:
-            tabs["Repr tree"] = mk.MkTreeView(node)
-        items = [mktabs.MkTab(content=v, title=k, parent=self) for k, v in tabs.items()]
-        items[0].new = True
-        if self.select_tab is not None:
-            pos = (
-                self._get_tab_pos(self.select_tab)
-                if isinstance(self.select_tab, str)
-                else self.select_tab
-            )
-            items[pos].select = True
-        return items
-
-    @items.setter
-    def items(self, value):
-        pass
+                return self._node
 
     @classmethod
     def create_example_page(cls, page):
