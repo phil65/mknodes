6ea188446ae2564d83731a636329c90f528105ef
phil65 <philipptemminghoff@googlemail.com>
2023-11-12 20:15:51 +0100

    refactor: make MkImageCompare inherit from MkTemplate

--- mknodes/basenodes/mkimagecompare/__init__.py
+++ mknodes/basenodes/mkimagecompare/__init__.py
@@ -2,11 +2,8 @@ from __future__ import annotations
 
 from typing import Any
 
-from mknodes.basenodes import mknode
-from mknodes.utils import log, resources, xmlhelpers as xml
-
-
-logger = log.get_logger(__name__)
+from mknodes.templatenodes import mktemplate
+from mknodes.utils import resources
 
 
 JS_URL = "https://unpkg.com/img-comparison-slider@7/dist/index.js"
@@ -14,11 +11,7 @@ JS_URL = "https://unpkg.com/img-comparison-slider@7/dist/index.js"
 CSS_URL = "https://unpkg.com/img-comparison-slider@7/dist/styles.css"
 
 
-class ImgComparisonSlider(xml.HTMLElement):
-    tag_name = "img-comparison-slider"
-
-
-class MkImageCompare(mknode.MkNode):
+class MkImageCompare(mktemplate.MkTemplate):
     """Node to show an Image comparison (using a slider)."""
 
     ICON = "material/image-off"
@@ -38,19 +31,9 @@ class MkImageCompare(mknode.MkNode):
             after_image: Image shown with slider to the left
             kwargs: Keyword arguments passed to parent
         """
-        super().__init__(**kwargs)
+        super().__init__("output/html/template", **kwargs)
         self.before_image = before_image
         self.after_image = after_image
-
-    def get_element(self) -> ImgComparisonSlider:
-        root = ImgComparisonSlider()
-        xml.Img(src=self.before_image, slot="first", parent=root)
-        xml.Img(src=self.after_image, slot="second", parent=root)
-        return root
-
-    def _to_markdown(self) -> str:
-        root = self.get_element()
-        return root.to_string()
 
     @classmethod
     def create_example_page(cls, page):
@@ -65,3 +48,4 @@ class MkImageCompare(mknode.MkNode):
 
 if __name__ == "__main__":
     img = MkImageCompare("a", "b")
+    print(img)
