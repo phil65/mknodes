9b562477d5f5dc29d3fed294cab7c5a37864165c
phil65 <philipptemminghoff@googlemail.com>
2023-11-14 21:24:38 +0100

    fix(GitRepository): pass kwargs to super() in init

--- mknodes/info/gitrepository.py
+++ mknodes/info/gitrepository.py
@@ -19,8 +19,8 @@ logger = log.get_logger(__name__)
 class GitRepository(git.Repo):
     """Aggregates information about a git repo."""
 
-    def __init__(self, path: str | os.PathLike | None = None):
-        super().__init__(path)
+    def __init__(self, path: str | os.PathLike | None = None, **kwargs):
+        super().__init__(path, **kwargs)
         # to keep a reference to a TempDirectory instance
         self.temp_directory: tempfile.TemporaryDirectory | None = None
 
