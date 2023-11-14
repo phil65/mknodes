71ccf2382acbabe70fa202658151c55c42b272e1
phil65 <philipptemminghoff@googlemail.com>
2023-11-14 23:18:00 +0100

    chore: test work

--- tests/test_gitrepository.py
+++ tests/test_gitrepository.py
@@ -8,7 +8,7 @@ from mknodes.info import gitrepository
 
 
 @pytest.mark.skipif(not bool(os.getenv("CI")), reason="Only run Git tests in CI")
-def test_getting_version_for_commit():
+def test_gitrepository():
     repo = gitrepository.GitRepository.clone_from(
         "https://github.com/phil65/mknodes",
         "testclone",
@@ -16,3 +16,9 @@ def test_getting_version_for_commit():
     assert repo.get_version_for_commit("fdd6a0f6") == "v0.49.5"  # one commit before bump
     assert repo.get_version_for_commit("82b61f02") == "v0.49.5"  # bump commit
     assert repo.get_version_for_commit("0a12a015") == "v0.49.6"  # one commit after bump
+    assert repo.main_branch == "main"
+    assert repo.repo_name == "mknodes"
+    assert repo.repo_url == "https://github.com/phil65/mknodes/"
+    assert "v0.49.5" in repo.version_changes
+    assert repo.code_repository == "GitHub"
+    assert repo.edit_uri == "edit/main/"
