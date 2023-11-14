8fa1ba684d20f217e72688e13661996c9d76a5a6
phil65 <philipptemminghoff@googlemail.com>
2023-11-14 23:12:00 +0100

    tests: activate GitRepository tests in CI

--- tests/test_gitrepository.py
+++ tests/test_gitrepository.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
-# from mknodes.info import gitrepository
+import os
 
-# fails in CI for the tagged commit
+import pytest
 
-# def test_getting_version_for_commit():
-#     repo = gitrepository.GitRepository(".")
-#     assert repo.get_version_for_commit("fdd6a0f6") == "v0.49.5"  # one commit before bump
-#     assert repo.get_version_for_commit("82b61f02") == "v0.49.5"  # bump commit
-#     assert repo.get_version_for_commit("0a12a015") == "v0.49.6"  # one commit after bump
+from mknodes.info import gitrepository
+
+
+@pytest.mark.skipif(not bool(os.getenv("CI")), reason="Only run Git tests in CI")
+def test_getting_version_for_commit():
+    repo = gitrepository.GitRepository.clone_from(
+        "https://github.com/phil65/mknodes",
+        "testclone",
+    )
+    assert repo.get_version_for_commit("fdd6a0f6") == "v0.49.5"  # one commit before bump
+    assert repo.get_version_for_commit("82b61f02") == "v0.49.5"  # bump commit
+    assert repo.get_version_for_commit("0a12a015") == "v0.49.6"  # one commit after bump
