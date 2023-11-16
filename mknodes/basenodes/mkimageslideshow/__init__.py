6614f28280aa8288c6ee9eef11aba89055251093
phil65 <philipptemminghoff@googlemail.com>
2023-11-16 18:40:58 +0100

    refactor: make MkImageSlideshow inherit from MkTemplate

--- mknodes/basenodes/mkimageslideshow/__init__.py
+++ mknodes/basenodes/mkimageslideshow/__init__.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import Any
 
-from mknodes.basenodes import mknode
-from mknodes.utils import log, resources, xmlhelpers as xml
+from mknodes.templatenodes import mktemplate
+from mknodes.utils import log, resources
 
 
 logger = log.get_logger(__name__)
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', function () {
 """
 
 
-class MkImageSlideshow(mknode.MkNode):
+class MkImageSlideshow(mktemplate.MkTemplate):
     """Node to show an Image slideshow (in autoplay mode)."""
 
     ICON = "material/image-multiple"
@@ -36,20 +36,10 @@ class MkImageSlideshow(mknode.MkNode):
             images: A list of image URLs to use for the slideshow
             kwargs: Keyword arguments passed to parent
         """
-        super().__init__(**kwargs)
+        super().__init__("output/html/template", **kwargs)
         self.images = images
-
-    def get_element(self) -> xml.Div:
-        attrs = {"data-simple-slider": ""}
-        root = xml.Div(style="width: 100%; padding-bottom: 25%", parent=None, **attrs)
-        for img in self.images:
-            xml.Img(src=img, parent=root)
-        return root
-
-    def _to_markdown(self) -> str:
-        root = self.get_element()
-        return root.to_string()
 
 
 if __name__ == "__main__":
     img = MkImageSlideshow(["https://picsum.photos/200", "https://picsum.photos/201"])
+    print(img)
