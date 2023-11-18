e1e45fab90ec0c6aa816114d8bda3c0cdfffbee6
phil65 <philipptemminghoff@googlemail.com>
2023-11-18 06:27:47 +0100

    chore: cleanup

--- tests/test_build.py
+++ tests/test_build.py
@@ -3,13 +3,6 @@ from __future__ import annotations
 import mknodes as mk
 
 
-def build(project):
-    sub_nav = mk.MkNav("Sub nav")
-    sub_nav.page_template.announcement_bar = "Hello"
-    sub_nav += mk.MkPage("Test page")
-    project.root += sub_nav
-
-
 def test_build():
     from mknodes.manual import root
 
