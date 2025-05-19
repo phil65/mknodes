d75e3d2ee4351e6f4cfdcdb9415bc498ac311734
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-05-19 17:40:13 +0200

    chore: catch exceptions when importing filters

--- mknodes/jinja/__init__.py
+++ mknodes/jinja/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import functools
+import logging
 
 import jinjarope
 import tomli_w
@@ -12,6 +13,8 @@ import yamling
 from mknodes import paths
 from mknodes.utils import classhelpers, inspecthelpers
 
+logger = logging.getLogger(__name__)
+
 
 def setup_env(env: jinjarope.Environment):
     """Used as extension point for the jinjarope environment.
@@ -19,17 +22,20 @@ def setup_env(env: jinjarope.Environment):
     Args:
         env: The jinjarope environment to extend
     """
-    node_klasses = get_nodes()
-    env.globals |= dict(mk=node_klasses, _mk=node_klasses)
-    env.globals |= {"inspecthelpers": inspecthelpers, "classhelpers": classhelpers}
-    path = paths.RESOURCES / "filters.toml"
-    env.load_jinja_file(path)
-    env.filters |= node_klasses
-    env.filters |= {
-        "dump_yaml": yamling.dump_yaml,
-        "dump_toml": tomli_w.dumps,
-        "load_yaml": yamling.load_yaml,
-    }
+    try:
+        node_klasses = get_nodes()
+        env.globals |= dict(mk=node_klasses, _mk=node_klasses)
+        env.globals |= {"inspecthelpers": inspecthelpers, "classhelpers": classhelpers}
+        path = paths.RESOURCES / "filters.toml"
+        env.load_jinja_file(path)
+        env.filters |= node_klasses
+        env.filters |= {
+            "dump_yaml": yamling.dump_yaml,
+            "dump_toml": tomli_w.dumps,
+            "load_yaml": yamling.load_yaml,
+        }
+    except Exception as e:  # noqa: BLE001
+        logger.warning("Could not setup jinjarope environment: %s", e)
 
 
 @functools.cache
