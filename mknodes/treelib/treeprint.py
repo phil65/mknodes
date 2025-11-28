f096460b48ba33d3f331b0f4367179ba9f2a06aa
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-28 20:19:02 +0100

    chore: typing

--- mknodes/treelib/treeprint.py
+++ mknodes/treelib/treeprint.py
@@ -1,24 +1,30 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from mknodes.utils import connector, log
+
+
+if TYPE_CHECKING:
+    import mknodes as mk
 
 
 logger = log.get_logger(__name__)
 
 
-class NodeConnector(connector.Connector):
-    def get_children(self, item):
+class NodeConnector(connector.Connector["mk.MkNode"]):
+    def get_children(self, item: mk.MkNode):  # pyright: ignore[reportIncompatibleMethodOverride]
         return item.get_children()
 
-    def get_id(self, item) -> str:
+    def get_id(self, item: mk.MkNode) -> str:
         # id() would be enough, but name is sometimes useful for debugging.
         return f"{type(item).__name__}_{id(item)}"
 
-    def get_title(self, item) -> str:
+    def get_title(self, item: mk.MkNode) -> str:
         return f"{type(item).__name__}"
 
 
-def to_tree_graph(node, direction: str = "TD") -> str:
+def to_tree_graph(node: mk.MkNode, direction: str = "TD") -> str:
     """Returns markdown to display a tree graph of this node and all subnodes.
 
     Args:
